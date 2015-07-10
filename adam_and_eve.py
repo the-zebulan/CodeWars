3c3d20fcd222b8433c1f22a17dd2a4098c3948bb
G <the-zebulan@users.noreply.github.com>
2015-07-10 13:44:47 -0600

    Added 'adam_and_eve'

---
+++ adam_and_eve.py
@@ -0,0 +1,19 @@
+class Human:
+    def __init__(self):
+        pass
+
+
+class Man(Human):
+    pass
+
+
+class Woman(Human):
+    pass
+
+
+def God():
+    """ god == PEP8 (forced to capitalize by codewars) """
+    return [Man(), Woman()]
+
+paradise = God()
+assert isinstance(paradise[0], Man) is True
