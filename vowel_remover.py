be37341a82fd594f358609b4df6922de42cd56e1
G <the-zebulan@users.noreply.github.com>
2015-07-10 12:40:55 -0600

    Added 'vowel_remover'.

---
+++ vowel_remover.py
@@ -0,0 +1,11 @@
+LOWER_VOWELS = 'aeiou'
+
+
+# def shortcut(string):
+#     return filter(lambda a: a not in LOWER_VOWELS, string)
+
+
+def shortcut(string):
+    return string.translate(None, LOWER_VOWELS)
+
+assert shortcut('hello') == 'hll'
