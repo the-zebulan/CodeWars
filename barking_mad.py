9f0161d3d408ea0ce90ed812f4e68a1ba29c1981
G <the-zebulan@users.noreply.github.com>
2015-07-10 13:24:47 -0600

    Minor refactor, made bark() a static method.

--- barking_mad.py
+++ barking_mad.py
@@ -1,8 +1,9 @@
-class Dog ():
+class Dog:
     def __init__(self, breed):
         self.breed = breed
 
-    def bark(self):
+    @staticmethod
+    def bark():
         return 'Woof'
 
 snoopy = Dog("Beagle")
