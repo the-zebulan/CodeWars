5e37780f108bfa2c8f665bfce508e5916af65b77
G <the-zebulan@users.noreply.github.com>
2015-07-10 21:30:53 -0600

    PEP8

--- welcome_to_the_city.py
+++ welcome_to_the_city.py
@@ -1,6 +1,9 @@
 def say_hello(name, city, state):
     return 'Hello, {}! Welcome to {}, {}!'.format(' '.join(name), city, state)
 
-assert say_hello(['John', 'Smith'], 'Phoenix', 'Arizona') ==  'Hello, John Smith! Welcome to Phoenix, Arizona!','Hello, John Smith! Welcome to Phoenix, Arizona!'
-assert say_hello(['Franklin','Delano','Roosevelt'], 'Chicago', 'Illinois') == 'Hello, Franklin Delano Roosevelt! Welcome to Chicago, Illinois!'
-assert say_hello(['Wallace','Russel','Osbourne'],'Albany','New York') == 'Hello, Wallace Russel Osbourne! Welcome to Albany, New York!'
+assert say_hello(['John', 'Smith'], 'Phoenix', 'Arizona') \
+    == 'Hello, John Smith! Welcome to Phoenix, Arizona!'
+assert say_hello(['Franklin', 'Delano', 'Roosevelt'], 'Chicago', 'Illinois') \
+    == 'Hello, Franklin Delano Roosevelt! Welcome to Chicago, Illinois!'
+assert say_hello(['Wallace', 'Russel', 'Osbourne'], 'Albany', 'New York') \
+    == 'Hello, Wallace Russel Osbourne! Welcome to Albany, New York!'
