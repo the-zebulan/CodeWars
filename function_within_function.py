948165d654d28bf8ca7b528d8648aa5064fd3477
G <the-zebulan@users.noreply.github.com>
2015-07-10 20:39:29 -0600

    Refactored using lambda

--- function_within_function.py
+++ function_within_function.py
@@ -1,7 +1,11 @@
+# def always(n=0):
+#     def closure():
+#         return n
+#     return closure
+
+
 def always(n=0):
-    def closure():
-        return n
-    return closure
+    return lambda: n
 
 three = always(3)
 assert three() == 3
