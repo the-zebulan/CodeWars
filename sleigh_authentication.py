39c91d15af73d363c1055903922ed8abbe16abea
G <the-zebulan@users.noreply.github.com>
2015-07-11 14:34:58 -0600

    Added 'sleigh_authentication'

---
+++ sleigh_authentication.py
@@ -0,0 +1,9 @@
+class Sleigh:
+    def authenticate(self, name, password):
+        return name == 'Santa Claus' and password == 'Ho Ho Ho!'
+
+sleigh = Sleigh()
+assert sleigh.authenticate('Santa Claus', 'Ho Ho Ho!') is True
+assert sleigh.authenticate('Santa', 'Ho Ho Ho!') is False
+assert sleigh.authenticate('Santa Claus', 'Ho Ho!') is False
+assert sleigh.authenticate('jhoffner', 'CodeWars') is False
