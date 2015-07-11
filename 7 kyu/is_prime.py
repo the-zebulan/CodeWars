e1ea06d0c8d8d5f668b1222fd0987001e12b0f84
G <the-zebulan@users.noreply.github.com>
2015-07-11 16:07:37 -0600

    Added 'is_prime'

---
+++ 7 kyu/is_prime.py
@@ -0,0 +1,13 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for x in range(2, n):
+        if n % x == 0:
+            return False
+    return True
+
+assert is_prime(0) is False
+assert is_prime(1) is False
+assert is_prime(2) is True
+assert is_prime(11) is True
+assert is_prime(12) is False
