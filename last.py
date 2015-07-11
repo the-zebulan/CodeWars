7784bfc57e11857be1e2c369c73120922eaab465
G <the-zebulan@users.noreply.github.com>
2015-07-10 18:34:38 -0600

    Added 'last' -> Incomplete

---
+++ last.py
@@ -0,0 +1,8 @@
+def last(*args):
+    end = args[-1]
+    return end[-1] if isinstance(end, (str, list, tuple)) else end
+
+assert last([1, 2, 3, 4, 5]) == 5
+assert last('abcde') == 'e'
+assert last(1, 'b', 3, 'd', 5) == 5
+assert last((['a', 'b', 'c', 'k', 'x', 'y', 'z'],), ['a', 'b', 'c', 'k', 'x', 'y', 'z']) == 'z'
