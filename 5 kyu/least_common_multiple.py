e1b4d06ef058ad0fa2ac52b54feb0ab22491f409
G <the-zebulan@users.noreply.github.com>
2015-07-11 15:23:02 -0600

    Refactored thanks to a solution from CodeWars

--- 5 kyu/least_common_multiple.py
+++ 5 kyu/least_common_multiple.py
@@ -1,14 +1,20 @@
+# def lcm(*args):
+#     """Compute the least common multiple of some non-negative integers"""
+#     args = set(args)  # remove duplicates
+#     if 0 in args:
+#         return 0
+#     x = max(args)
+#     y = x
+#     args.remove(x)
+#     while any(x % z for z in args):
+#         x += y
+#     return x
+
+from fractions import gcd
+
+
 def lcm(*args):
-    """Compute the least common multiple of some non-negative integers"""
-    args = set(args)  # remove duplicates
-    if 0 in args:
-        return 0
-    x = max(args)
-    y = x
-    args.remove(x)
-    while any(x % z for z in args):
-        x += y
-    return x
+    return reduce(lambda x, y: x * y / gcd(x, y), args)
 
 assert lcm(2, 5) == 10
 assert lcm(2, 3, 4) == 12
