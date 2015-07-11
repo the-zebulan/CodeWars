1523c97e7b4a27be70f653437b31397668cf8f61
G <the-zebulan@users.noreply.github.com>
2015-07-10 18:54:21 -0600

    Minor refactor using lower()

--- playing_banjo.py
+++ playing_banjo.py
@@ -4,8 +4,7 @@ NOPE = 'does not play'
 
 def areYouPlayingBanjo(name):
     """ are_you_playing_banjo == PEP8 (forced camelCase by codewars) """
-    starts_with_r = name.startswith(('r', 'R'))
-    return '{} {} banjo'.format(name, PLAY if starts_with_r else NOPE)
+    return '{} {} banjo'.format(name, PLAY if name[0].lower() == 'r' else NOPE)
 
 assert areYouPlayingBanjo('martin') == 'martin does not play banjo'
 assert areYouPlayingBanjo('Rikke') == 'Rikke plays banjo'
