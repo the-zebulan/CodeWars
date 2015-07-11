303e3c1430041119ad4fff0e8a76c47a9d0c2a1d
G <the-zebulan@users.noreply.github.com>
2015-07-11 15:07:27 -0600

    Added 'get_planet_name_by_id'

---
+++ get_planet_name_by_id.py
@@ -0,0 +1,12 @@
+PLANETS = ('Mercury', 'Venus', 'Earth', 'Mars',
+           'Jupiter', 'Saturn', 'Uranus', 'Neptune')
+
+def get_planet_name(id):
+    return PLANETS[id - 1]
+
+assert get_planet_name(2) == 'Venus'
+assert get_planet_name(5) == 'Jupiter'
+assert get_planet_name(3) == 'Earth'
+assert get_planet_name(4) == 'Mars'
+assert get_planet_name(8) == 'Neptune'
+assert get_planet_name(1) == 'Mercury'
