a31d8003f39e8966d37a9a6a60f90e9f80bdad09
G <the-zebulan@users.noreply.github.com>
2015-07-11 19:35:30 -0600

    Added 'fuel_economy_converter'

---
+++ 7 kyu/fuel_economy_converter.py
@@ -0,0 +1,14 @@
+LITRES_PER_GALLON = 3.785411784
+KMS_PER_MILE = 1.609344
+
+
+def mpg2lp100km(mpg):
+    """ mpg_to_litres_per_100km == PEP8 """
+    return round(LITRES_PER_GALLON / ((mpg * KMS_PER_MILE) / 100), 2)
+
+def lp100km2mpg(lp100km):
+    """ litres_per_100_km_to_mpg == PEP8 """
+    return round((100 / KMS_PER_MILE) / (lp100km / LITRES_PER_GALLON) , 2)
+
+assert mpg2lp100km(42) == 5.6
+assert lp100km2mpg(9) == 26.13
