6b3564fb60a6dad15e377c8cb370ae32485bb616
G <the-zebulan@users.noreply.github.com>
2015-07-11 23:10:16 -0600

    Added 'make_backronym'

---
+++ 7 kyu/make_backronym.py
@@ -0,0 +1,15 @@
+DICTIONARY = {'A': 'awesome', 'C': 'confident', 'B': 'beautiful',
+              'E': 'eager', 'D': 'disturbing', 'G': 'gregarious',
+              'F': 'fantastic', 'I': 'ingestable', 'H': 'hippy',
+              'K': 'klingon', 'J': 'joke', 'M': 'mustache', 'L': 'literal',
+              'O': 'oscillating', 'N': 'newtonian', 'Q': 'queen',
+              'P': 'perfect', 'S': 'stylish', 'R': 'rant', 'U': 'underlying',
+              'T': 'turn', 'W': 'weird', 'V': 'volcano', 'Y': 'yogic',
+              'X': 'xylophone', 'Z': 'zero'}
+
+
+def make_backronym(acronym):
+    return ' '.join(DICTIONARY[a.upper()] for a in acronym)
+
+assert make_backronym('interesting') == 'ingestable newtonian turn eager rant eager stylish turn ingestable newtonian gregarious'
+assert make_backronym('codewars') == 'confident oscillating disturbing eager weird awesome rant stylish'
