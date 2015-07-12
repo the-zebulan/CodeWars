e3006505f30a00ab026e099904bbcaa4ee9b86b5
G <the-zebulan@users.noreply.github.com>
2015-07-11 23:20:59 -0600

    Added a lambda version of the 'add' function

--- 7 kyu/gradually_adding_parameters.py
+++ 7 kyu/gradually_adding_parameters.py
@@ -1,6 +1,8 @@
 def add(*args):
     return sum(i * a for i, a in enumerate(args, start=1))
 
+# add = lambda *args: sum(i * a for i, a in enumerate(args, start=1))
+
 assert add() == 0
 assert add(100, 200, 300) == 1400
 assert add(2) == 2
