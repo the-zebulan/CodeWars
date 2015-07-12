f8ab5a70842d2455ff68a22b7a6abaf373b45080
G <the-zebulan@users.noreply.github.com>
2015-07-11 22:58:30 -0600

    Refactored thanks to a solution from Codewars (PEP8 newline)

--- 7 kyu/make_acronym.py
+++ 7 kyu/make_acronym.py
@@ -12,7 +12,8 @@
 
 def make_acronym(phrase):
     try:
-        return ''.join(word[0].upper() if word.isalpha() else 0 for word in phrase.split())
+        return ''.join(word[0].upper() if word.isalpha() else 0
+                       for word in phrase.split())
     except AttributeError:
         return 'Not a string'
     except TypeError:
