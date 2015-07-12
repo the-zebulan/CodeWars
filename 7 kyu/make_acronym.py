fb62cf6ca5d69fdb473dca4cd83cef7be9151c2a
G <the-zebulan@users.noreply.github.com>
2015-07-11 22:58:03 -0600

    Refactored thanks to a solution from Codewars

--- 7 kyu/make_acronym.py
+++ 7 kyu/make_acronym.py
@@ -1,13 +1,22 @@
+# def make_acronym(phrase):
+#     if not isinstance(phrase, str):
+#         return 'Not a string'
+#     words = phrase.split()
+#     result = []
+#     for word in words:
+#         if not word.isalpha():
+#             return 'Not letters'
+#         result.append(word[0].upper())
+#     return ''.join(result)
+
+
 def make_acronym(phrase):
-    if not isinstance(phrase, str):
+    try:
+        return ''.join(word[0].upper() if word.isalpha() else 0 for word in phrase.split())
+    except AttributeError:
         return 'Not a string'
-    words = phrase.split()
-    result = []
-    for word in words:
-        if not word.isalpha():
-            return 'Not letters'
-        result.append(word[0].upper())
-    return ''.join(result)
+    except TypeError:
+        return 'Not letters'
 
 assert make_acronym('Hello codewarrior') == 'HC'
 assert make_acronym('a42') == 'Not letters'
