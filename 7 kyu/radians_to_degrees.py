a58e350f52e101e537f9493d6417461ae7d28a2c
G <the-zebulan@users.noreply.github.com>
2015-07-12 15:17:38 -0600

    Refactored using lambdas

--- 7 kyu/radians_to_degrees.py
+++ 7 kyu/radians_to_degrees.py
@@ -2,15 +2,10 @@ import math
 
 DEGREES_PER_RADIAN = math.pi / 180.0
 RADIANS_PER_DEGREE = 180.0 / math.pi
+OUTPUT = '{:g}{}'.format
 
-
-def calculate(constant, variable, output):
-    result = round(constant * variable, 2)
-    r_int = int(result)
-    return '{}{}'.format(r_int if result == r_int else result, output)
-
-math.degrees = lambda rad: calculate(RADIANS_PER_DEGREE, rad, 'deg')
-math.radians = lambda deg: calculate(DEGREES_PER_RADIAN, deg, 'rad')
+math.degrees = lambda rad: OUTPUT(round(RADIANS_PER_DEGREE * rad, 2), 'deg')
+math.radians = lambda deg: OUTPUT(round(DEGREES_PER_RADIAN * deg, 2), 'rad')
 
 assert math.degrees(math.pi) == '180deg'
 assert math.radians(180) == '3.14rad'
