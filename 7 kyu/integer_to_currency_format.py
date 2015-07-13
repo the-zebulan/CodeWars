a5b062cf15d6cb54e3a2862435ed07efaa7e4d51
G <the-zebulan@users.noreply.github.com>
2015-07-13 12:18:59 -0600

    Added 'integer_to_currency_format'

---
+++ 7 kyu/integer_to_currency_format.py
@@ -0,0 +1,6 @@
+to_currency = lambda price: '{:,}'.format(price)
+
+assert to_currency(123456) == "123,456"
+assert to_currency(1234) == "1,234"
+assert to_currency(123) == "123"
+assert to_currency(123456789012) == "123,456,789,012"
