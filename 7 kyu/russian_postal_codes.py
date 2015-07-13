2e4b56ab5f2008eb59ae53229f8726c85d6b7272
G <the-zebulan@users.noreply.github.com>
2015-07-13 12:15:12 -0600

    Refactored using re.match() thanks to a solution from CodeWars

--- 7 kyu/russian_postal_codes.py
+++ 7 kyu/russian_postal_codes.py
@@ -1,13 +1,19 @@
-INVALID = {'0', '5', '7', '8', '9'}
+# INVALID = {'0', '5', '7', '8', '9'}
+#
+#
+# def zip_validate(postal_code):
+#     length = 0
+#     for i, a in enumerate(postal_code):
+#         if i == 0 and a in INVALID or not a.isdigit():
+#             return False
+#         length += 1
+#     return length == 6
+
+from re import match
 
 
-def zip_validate(postal_code):
-    length = 0
-    for i, a in enumerate(postal_code):
-        if i == 0 and a in INVALID or not a.isdigit():
-            return False
-        length += 1
-    return length == 6
+def zip_validate(postcode):
+    return bool(match("^[12346][0-9]{5}$", postcode))
 
 assert zip_validate('198328')
 assert zip_validate('310003')
