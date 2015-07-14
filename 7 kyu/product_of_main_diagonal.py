598f4f7d0b32cc3923196d5165ee1941a1d2ac76
G <the-zebulan@users.noreply.github.com>
2015-07-14 13:03:12 -0600

    PEP8

--- 7 kyu/product_of_main_diagonal.py
+++ 7 kyu/product_of_main_diagonal.py
@@ -4,5 +4,5 @@ from operator import mul
 def main_diagonal_product(matrix):
     return reduce(mul, (matrix[a][a] for a in xrange(len(matrix))))
 
-assert main_diagonal_product([[1,0],[0,1]]) == 1
-assert main_diagonal_product([[1,2,3],[4,5,6],[7,8,9]]) == 45
+assert main_diagonal_product([[1, 0], [0, 1]]) == 1
+assert main_diagonal_product([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) == 45
