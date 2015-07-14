a5e667a5ae41d83eec1af212afc5b5dabd421f3f
G <the-zebulan@users.noreply.github.com>
2015-07-13 22:18:41 -0600

    PEP8

--- 7 kyu/categorize_new_member.py
+++ 7 kyu/categorize_new_member.py
@@ -3,6 +3,6 @@ def openOrSenior(data):
     return ['Senior' if age >= 55 and h > 7 else 'Open' for age, h in data]
 
 assert openOrSenior([[45, 12], [55, 21], [19, -2], [104, 20]]) == \
-       ['Open', 'Senior', 'Open', 'Senior']
-assert openOrSenior([[16, 23],[73,1],[56, 20],[1, -1]]) == \
-       ['Open', 'Open', 'Senior', 'Open']
+    ['Open', 'Senior', 'Open', 'Senior']
+assert openOrSenior([[16, 23], [73, 1], [56, 20], [1, -1]]) == \
+    ['Open', 'Open', 'Senior', 'Open']
