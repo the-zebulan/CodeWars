8fd518b3294b8beb011d0c6bd0627ea3db86b811
G <the-zebulan@users.noreply.github.com>
2015-07-13 22:09:04 -0600

    Added 'html_generator'

---
+++ 7 kyu/html_generator.py
@@ -0,0 +1,40 @@
+HTML_TAG = '<{0}>{1}</{0}>'.format
+
+
+class HTMLGen:
+    @staticmethod
+    def comment(text):
+        return '<!--{}-->'.format(text)
+
+    @staticmethod
+    def a(text):
+        return HTML_TAG('a', text)
+
+    @staticmethod
+    def b(text):
+        return HTML_TAG('b', text)
+
+    @staticmethod
+    def p(text):
+        return HTML_TAG('p', text)
+
+    @staticmethod
+    def body(text):
+        return HTML_TAG('body', text)
+
+    @staticmethod
+    def div(text):
+        return HTML_TAG('div', text)
+
+    @staticmethod
+    def span(text):
+        return HTML_TAG('span', text)
+
+    @staticmethod
+    def title(text):
+        return HTML_TAG('title', text)
+
+test = HTMLGen()
+assert test.a('test') == '<a>test</a>'
+assert test.comment('test') == '<!--test-->'
+assert test.title('hmm') == '<title>hmm</title>'
