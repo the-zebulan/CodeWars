780164b579d6655b85975e4d45dc3e249cdbec8e
G <the-zebulan@users.noreply.github.com>
2015-07-14 11:14:22 -0600

    Added 'credit_card_mask'

---
+++ 7 kyu/credit_card_mask.py
@@ -0,0 +1,6 @@
+maskify = lambda cc: '{:#>{}}'.format(cc[-4:], len(cc))
+
+assert maskify('4556364607935616') == '############5616'
+assert maskify('64607935616') == '#######5616'
+assert maskify('1') == '1'
+assert maskify('') == ''
