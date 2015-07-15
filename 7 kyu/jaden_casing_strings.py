73fe2b2821a4c6b0f341bfa4aea8f4bca7b1a7f4
the-zebulan <the-zebulan@users.noreply.github.com>
2015-07-15 15:21:46 -0600

    Added 'jaden_casing_strings'

---
+++ 7 kyu/jaden_casing_strings.py
@@ -0,0 +1,6 @@
+def toJadenCase(string):
+    """ to_jaden_case == PEP8 """
+    return ' '.join(a.capitalize() for a in string.split())
+
+assert toJadenCase('How can mirrors be real if our eyes aren\'t real') == \
+       'How Can Mirrors Be Real If Our Eyes Aren\'t Real'
