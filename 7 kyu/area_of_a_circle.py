52f612047f539c7facb9ddc57447a3f46e3fd98b
the-zebulan <the-zebulan@users.noreply.github.com>
2015-07-15 14:59:48 -0600

    PEP8

--- 7 kyu/area_of_a_circle.py
+++ 7 kyu/area_of_a_circle.py
@@ -2,6 +2,7 @@ from math import pi
 
 
 def circleArea(r):
+    """ circle_area == PEP8 (forced camelCase by codewars) """
     if r <= 0 or not isinstance(r, (int, float)):
         return False
     return round(pi * (r ** 2), 2)
