2391c72e0f7ce781af6f0f3477f0d3e9acdb41fa
the-zebulan <the-zebulan@users.noreply.github.com>
2015-07-15 16:02:11 -0600

    Added 'fizz_buzz'

---
+++ 7 kyu/fizz_buzz.py
@@ -0,0 +1,12 @@
+def fizzbuzz(n):
+    result = []
+    for a in range(1, n + 1):
+        current = ''
+        if a % 3 == 0:
+            current += 'Fizz'
+        if a % 5 == 0:
+            current += 'Buzz'
+        result.append(current if current else a)
+    return result
+
+assert fizzbuzz(10) == [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz']
