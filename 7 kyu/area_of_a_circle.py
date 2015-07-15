aaef997b119f8fc4e28a7e56913af0f907477723
the-zebulan <the-zebulan@users.noreply.github.com>
2015-07-15 14:58:58 -0600

    Added 'area_of_a_circle'

---
+++ 7 kyu/area_of_a_circle.py
@@ -0,0 +1,13 @@
+from math import pi
+
+
+def circleArea(r):
+    if r <= 0 or not isinstance(r, (int, float)):
+        return False
+    return round(pi * (r ** 2), 2)
+
+assert circleArea(-1485.86) is False
+assert circleArea(0) is False
+assert circleArea(43.2673) == 5881.25
+assert circleArea(68) == 14526.72
+assert circleArea("number") is False
