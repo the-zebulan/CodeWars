69447c848d64db89af13c7adc4e8b96f64e0df01
the-zebulan <the-zebulan@users.noreply.github.com>
2015-07-15 15:33:04 -0600

    Minor refactor and added two tests

--- 7 kyu/unlimited_sum.py
+++ 7 kyu/unlimited_sum.py
@@ -1,2 +1,9 @@
 def sum(*args):
     return reduce(lambda a, b: a + b, (c for c in args if isinstance(c, int)))
+
+
+# def sum(*args):
+#     return __builtins__.sum(filter(lambda a: isinstance(a, int), args))
+
+assert sum(1, 2, 3) == 6
+assert sum('1', 1, '2', 2) == 3
