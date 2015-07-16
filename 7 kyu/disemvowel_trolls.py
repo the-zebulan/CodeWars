ec6d045fb6a09b67a828bcb1d26f77767e33356b
the-zebulan <the-zebulan@users.noreply.github.com>
2015-07-15 22:00:47 -0600

    Added 'disemvowel_trolls'

---
+++ 7 kyu/disemvowel_trolls.py
@@ -0,0 +1,11 @@
+# VOWELS = 'aeiou'
+#
+#
+# def disemvowel(string):
+#     return ''.join(a for a in string if not a.lower() in VOWELS)
+
+
+def disemvowel(s):
+    return s.translate(None, "aeiouAEIOU")
+
+assert disemvowel('This website is for losers LOL!') == 'Ths wbst s fr lsrs LL!'
