9c11cccf214c4c10b86dd45a39f1bcea0bbe8eb4
the-zebulan <the-zebulan@users.noreply.github.com>
2015-07-16 16:15:57 -0600

    Added 'leap_years'

---
+++ 7 kyu/leap_years.py
@@ -0,0 +1,8 @@
+def isLeapYear(year):
+    """ is_leap_year == PEP8, forced camelCase by codewars """
+    return year % 4 == 0 and not year % 100 == 0 or year % 400 == 0
+
+assert isLeapYear(1984) is True
+assert isLeapYear(2000) is True
+assert isLeapYear(1234) is False
+assert isLeapYear(1100) is False
