c035b56f2653a6ed4caccd88fbb6005f088d65d3
the-zebulan <the-zebulan@users.noreply.github.com>
2015-07-15 22:07:21 -0600

    Added 'summing_a_numbers_digits'

---
+++ 7 kyu/summing_a_numbers_digits.py
@@ -0,0 +1,7 @@
+def sumDigits(number):
+    """ sum_digits == PEP8 (camelCase forced by codewars) """
+    return sum(map(int, str(abs(number))))
+
+assert sumDigits(10) == 1
+assert sumDigits(99) == 18
+assert sumDigits(-32) == 5
