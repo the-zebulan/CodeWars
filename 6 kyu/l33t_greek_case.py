0a9ac0fd38a1ff7ba54cb89eeaea3a15e93ce45e
the-zebulan <the-zebulan@users.noreply.github.com>
2015-07-17 22:06:52 -0600

    Added 'l33t_greek_case'

---
+++ 6 kyu/l33t_greek_case.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+table = {'A': 'α', 'B': 'β', 'D': 'δ', 'E': 'ε', 'I': 'ι', 'K': 'κ',
+         'N': 'η', 'O': 'θ', 'P': 'ρ', 'R': 'π', 'T': 'τ', 'U': 'μ',
+         'V': 'υ', 'W': 'ω', 'X': 'χ', 'Y': 'γ'}
+
+
+def gr33k_l33t(string):
+    result = []
+    for a in string.upper():
+        try:
+            result.append(table[a])
+        except KeyError:
+            result.append(a.lower())
+    return ''.join(result)
+
+assert gr33k_l33t("CodeWars") == "cθδεωαπs"
+assert gr33k_l33t("Kata") == "κατα"
