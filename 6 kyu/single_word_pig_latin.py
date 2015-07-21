63ffaeee5b6181a69dc975305c75c762ae23c885
the-zebulan <the-zebulan@users.noreply.github.com>
2015-07-20 22:59:56 -0600

    Added 'single_word_pig_latin'

---
+++ 6 kyu/single_word_pig_latin.py
@@ -0,0 +1,23 @@
+OUTPUT = '{}{}'.format
+VOWELS = 'aeiou'
+
+
+def pig_latin(string):
+    if not string.isalpha() or not string:
+        return None
+    string = string.lower()
+    first_vowel = None
+    for i, a in enumerate(string):
+        if first_vowel is None and a in VOWELS:
+            first_vowel = i
+            break
+
+    if first_vowel is None:
+        return OUTPUT(string, 'ay')
+    if first_vowel == 0:
+        return OUTPUT(string, 'way')
+    return OUTPUT(string[first_vowel:] + string[:first_vowel], 'ay')
+
+assert pig_latin("map") == "apmay"
+assert pig_latin("egg") == "eggway"
+assert pig_latin("spaghetti") == "aghettispay"
