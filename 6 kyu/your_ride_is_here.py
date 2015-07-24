f8bd74ce0f79871e5ba755e78940c7d92b71729b
the-zebulan <the-zebulan@users.noreply.github.com>
2015-07-24 14:34:27 -0600

    Added 'your_ride_is_here'

---
+++ 6 kyu/your_ride_is_here.py
@@ -0,0 +1,14 @@
+from string import ascii_uppercase
+
+AZ = dict(zip(ascii_uppercase, xrange(1, 27)))
+
+
+def product_mod_47(seq):
+    return reduce(lambda a, b: a * b, (AZ[c] for c in seq)) % 47
+
+
+def ride(group, comet):
+    return 'GO' if product_mod_47(group) == product_mod_47(comet) else 'STAY'
+
+assert ride('COMETQ', 'HVNGAT') == 'GO'
+assert ride('STARAB', 'USACO') == 'STAY'
