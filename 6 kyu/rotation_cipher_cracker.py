3fc02307c71c51d1989ee87ff2cb3e8a7ae99165
the-zebulan <the-zebulan@users.noreply.github.com>
2015-07-25 13:37:17 -0600

    Added 'rotation_cipher_cracker'

---
+++ 6 kyu/rotation_cipher_cracker.py
@@ -0,0 +1,19 @@
+from string import ascii_lowercase as az, maketrans
+
+TRANSLATIONS = [maketrans(az, az[a:] + az[:a]) for a in range(1, 26)]
+
+
+def decode(msg, contents):
+    result = []
+    for b in TRANSLATIONS:
+        tmp = msg.translate(b)
+        if contents in tmp:
+            result.append(tmp)
+    return result
+
+assert decode('ymjxvznwwjqnxhzyj', 'squirrel') == ['thesquirreliscute']
+assert decode('lzwespnsdmwakafxafalq', 'max') == \
+    ['maxftqotenxblbgybgbmr', 'themaxvalueisinfinity']
+decoded_msgs = decode('lzwespnsdmwakafxafalq', 'max')
+assert 'maxftqotenxblbgybgbmr' in decoded_msgs and \
+       'themaxvalueisinfinity' in decoded_msgs
