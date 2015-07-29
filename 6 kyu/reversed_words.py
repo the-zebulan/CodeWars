f77550edfdceb75f4b4803ea0fc5674d7fc73b6e
the-zebulan <the-zebulan@users.noreply.github.com>
2015-07-29 11:20:28 -0600

    Added 'reversed_words'

---
+++ 6 kyu/reversed_words.py
@@ -0,0 +1,7 @@
+def reverseWords(string):
+    """ reverse_words == PEP8, forced camelCase by CodeWars """
+    return ' '.join(reversed(string.split()))
+
+assert reverseWords('The greatest victory is that which requires no battle') \
+       == 'battle no requires which that is victory greatest The'
+assert reverseWords('hello world!') == 'world! hello'
