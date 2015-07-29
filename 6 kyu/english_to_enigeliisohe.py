ca099e4034b0676cd5f5e3fcc2895ea226e51598
the-zebulan <the-zebulan@users.noreply.github.com>
2015-07-28 22:42:47 -0600

    Added 'english_to_enigeliisohe'

---
+++ 6 kyu/english_to_enigeliisohe.py
@@ -0,0 +1,20 @@
+VOWELS = 'aeiou'
+PREV_VOWEL = {'b': 'a', 'c': 'a', 'd': 'a', 'f': 'e', 'g': 'e', 'h': 'e',
+              'j': 'i', 'k': 'i', 'l': 'i', 'm': 'i', 'n': 'i', 'p': 'o',
+              'q': 'o', 'r': 'o', 's': 'o', 't': 'o', 'v': 'u', 'w': 'u',
+              'x': 'u', 'y': 'u', 'z': 'u'}
+
+
+def toexuto(text):
+    result = []
+    for a in text:
+        low = a.lower()
+        if a.isalpha() and low not in VOWELS:
+            result.append('{}{}'.format(a, PREV_VOWEL[low]))
+        else:
+            result.append(a)
+    return ''.join(result)
+
+assert toexuto('little') == 'liitotolie'
+assert toexuto('BIG') == 'BaIGe'
+assert toexuto('This is a test. This is only a test.') == 'Toheiso iso a toesoto. Toheiso iso oniliyu a toesoto.'
