e8e4f90e536606a61e9da1f517965e44fa9206c7
the-zebulan <the-zebulan@users.noreply.github.com>
2015-07-31 16:44:42 -0600

    Added 'bit_counting'

---
+++ 6 kyu/bit_counting.py
@@ -0,0 +1,9 @@
+def countBits(n):
+    """ count_bits == PEP8, forced camelCase by CodeWars """
+    return '{:b}'.format(n).count('1')
+
+assert countBits(0) == 0
+assert countBits(4) == 1
+assert countBits(7) == 3
+assert countBits(9) == 2
+assert countBits(10) == 2
