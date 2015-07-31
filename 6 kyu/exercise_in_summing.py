f8a2be871e5c4f8c2b9f70e9530c4ef4e0c98cff
the-zebulan <the-zebulan@users.noreply.github.com>
2015-07-31 11:30:47 -0600

    Minor refactor. Removed unnecessary checks.

--- 6 kyu/exercise_in_summing.py
+++ 6 kyu/exercise_in_summing.py
@@ -1,21 +1,13 @@
 def maximum_sum(values, n):
-    if not values or n <= 0:
-        return 0
-    length = len(values)
-    if n > length:
-        n = length
     values.sort()
     return sum(values[-n:])
 
 
 def minimum_sum(values, n):
-    if not values or n <= 0:
-        return 0
-    length = len(values)
-    if n > length:
-        n = length
     values.sort()
     return sum(values[:n])
 
 assert minimum_sum([5, 4, 3, 2, 1], 2) == 3
 assert maximum_sum([5, 4, 3, 2, 1], 3) == 12
+assert minimum_sum([5, 4, 3, 2, 1], 7) == 15
+assert minimum_sum([], 3) == 0
