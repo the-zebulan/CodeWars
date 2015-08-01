c0786ae5be4413c6aec83e3c8890acb47d9c8911
the-zebulan <the-zebulan@users.noreply.github.com>
2015-07-31 21:18:34 -0600

    Added 'round_by_0.5_steps'

---
+++ 6 kyu/round_by_0.5_steps.py
@@ -0,0 +1,9 @@
+def solution(n):
+    return round(n * 2) / 2.0
+
+assert solution(4.2) == 4
+assert solution(4.25) == 4.5
+assert solution(4.4) == 4.5
+assert solution(4.6) == 4.5
+assert solution(4.75) == 5
+assert solution(4.8) == 5
