6c996032233dcf5151a5188bb07325b9ad1ba061
the-zebulan <the-zebulan@users.noreply.github.com>
2015-08-15 13:23:34 -0600

    Added 'cis_122_simple_printing'

---
+++ Beta/8 kyu/cis_122_simple_printing.py
@@ -0,0 +1,17 @@
+# print "Hello World!"
+# print "Welcome to CIS 122: Intro to Software Design"
+# print "The sum of 1.1 and 3 is 4.1"
+#
+# ''' From CodeWars:
+# Since print on CodeWars is the Python 2.7 version of print, but we want to
+# get practice with the Python 3 version, I have included a command called
+# x_print. It works the same way as the print command works in Python 3.'''
+#
+# x_print("Hello World!")
+# x_print("Learning Python is Fun")
+# x_print("There are 80 slices in 10 pizzas.")
+# x_print("80: It must be dinner time!")
+
+
+# print "Hello World!\nWelcome to CIS 122: Intro to Software Design\nThe sum of 1.1 and 3 is 4.1"
+# x_print("Hello World!\nLearning Python is Fun\nThere are 80 slices in 10 pizzas.\n80: It must be dinner time!")
