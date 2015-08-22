7c5d5a250ab66fab8d6c45d749268932ba956780
the-zebulan <the-zebulan@users.noreply.github.com>
2015-08-22 12:54:54 -0600

    Renamed function to 'score_test' after my suggestion was accepted on CodeWars

--- Beta/7 kyu/scoring_tests.py
+++ Beta/7 kyu/scoring_tests.py
@@ -1,7 +1,6 @@
-def scoreTest(tests, right, omit, wrong):
-    """ score_test == PEP8, forced mixedCase by CodeWars """
+def score_test(tests, right, omit, wrong):
     points = (right, omit, -wrong)
     return sum(points[test] for test in tests)
 
-assert scoreTest([0, 0, 0, 0, 2, 1, 0], 2, 0, 1) == 9
-assert scoreTest([0, 1, 0, 0, 2, 1, 0, 2, 2, 1], 3, -1, 2) == 3
+assert score_test([0, 0, 0, 0, 2, 1, 0], 2, 0, 1) == 9
+assert score_test([0, 1, 0, 0, 2, 1, 0, 2, 2, 1], 3, -1, 2) == 3
