74c263a30b2870186652905eb78b6e2857f1f7a9
the-zebulan <the-zebulan@users.noreply.github.com>
2015-08-23 23:00:53 -0600

    Added 'trigrams'

---
+++ Beta/7 kyu/trigrams.py
@@ -0,0 +1,7 @@
+def trigrams(phrase):
+    phrase = phrase.replace(' ', '_')
+    return ' '.join(phrase[a:a + 3] for a in range(len(phrase) - 2))
+
+assert trigrams('the quick red') == \
+    'the he_ e_q _qu qui uic ick ck_ k_r _re red'
+assert not trigrams('Hi')
