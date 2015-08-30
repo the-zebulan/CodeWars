177a980c0a05e208503799c1a9b72bf8be915ee1
G <the-zebulan@users.noreply.github.com>
2015-08-30 15:27:14 -0600

    Added 'help_the_fruit_guy'

---
+++ Beta/help_the_fruit_guy.py
@@ -0,0 +1,9 @@
+def removeRotten(bagOfFruits):
+    # bag_of_fruits == PEP8, forced mixedCase by CodeWars
+    return [] if not bagOfFruits else \
+        [fruit.replace('rotten', '').lower() for fruit in bagOfFruits]
+
+assert removeRotten(['rottenApple', 'rottenBanana', 'rottenApple',
+                    'rottenPineapple', 'rottenKiwi']) == \
+    ['apple', 'banana', 'apple', 'pineapple', 'kiwi']
+assert removeRotten([]) == []
