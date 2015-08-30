61e9e5787bf0f55233e6ef15cd01b48db7077477
G <the-zebulan@users.noreply.github.com>
2015-08-29 23:00:09 -0600

    Added 'cis_122_simple_operations'

---
+++ Beta/cis_122_simple_operations.py
@@ -0,0 +1,79 @@
+print '''\
+What is the width of the rectangle?
+What is the height of the rectangle?
+The area is: 7.5
+What is the width of the rectangle?
+What is the height of the rectangle?
+The area is: 7.5
+The sum is: 7
+7 3 10
+2
+1 25
+True False
+What is the radius?
+The area of the circle is: 95.03317615
+What is the radius of the sphere?
+The volume of the sphere is: 1150.34649037
+John Smith earned a total of 163.8 points.
+3.5: 4, 4.5
+False True True
+True False
+True False False
+Are you happy and you know it (Y/N)?
+y
+Are you happy and you know it (Y/N)?
+False'''
+
+input('')
+input('')
+input('')
+input('')
+input('')
+input('')
+input('')
+input('')
+
+a = 5
+a_divided_by_b = 2
+a_minus_b = 3
+a_modulo_b = 1
+a_plus_b = 7
+a_times_b = 10
+a_to_the_power_of_b = 25
+answered_yes = False
+area = 7.5
+b = 2
+circle_area = 95.03317615
+decimal_num = 4.5
+first = 2
+first_name = "John"
+five_is_even = False
+five_is_odd = True
+full_name = "John Smith"
+height = 3.0
+last_name = "Smith"
+num_string = "3.5"
+radius = 6.5
+score_one = 75.5
+score_two = 88.3
+second = 3
+sum = 7
+third = 2
+total_score = 163.8
+v1 = 2.5
+v1_between_2_and_3 = True
+v1_equals_v1 = True
+v1_equals_v2 = False
+v1_greater_than_or_equal_to_v2 = False
+v1_less_than_v2 = True
+v1_not_equals_v2 = True
+v1_not_less_than_v2 = False
+v2 = 3.5
+v2_less_than_3_or_over_4 = False
+volume = 1150.3464903666666
+whole_num = 4
+width = 2.5
+x = 2.5
+y = 3.0
+y_or_n_string = "N"
+z = 7.5
