4a94d6c4a2af61f9de609dec0f23d3ce2a7da121
G <the-zebulan@users.noreply.github.com>
2015-09-25 22:18:19 -0600

    Added 'find_smallest_int'

---
+++ 7 kyu/find_smallest_int.py
@@ -0,0 +1,6 @@
+def findSmallestInt(arr):
+    # find_smallest_int == PEP8, forced mixedCase by CodeWars
+    return min(arr)
+
+assert findSmallestInt([78, 56, 232, 12, 11, 43]) == 11
+assert findSmallestInt([78, 56, -2, 12, 8, -33]) == -33
