22cbeded38082291f33ac3db1addd5ee4044ff29
G <the-zebulan@users.noreply.github.com>
2015-09-26 20:28:37 -0600

    Minor refactor, removed abs() and uniq variable

--- Beta/how_many_numbers.py
+++ Beta/how_many_numbers.py
@@ -2,9 +2,8 @@ def sel_number(n, d):
     cnt = 0
     for a in range(12, n + 1):
         nums = map(int, str(a))
-        uniq = sorted(set(nums))
-        if nums == uniq and all(abs(b - c) <= d
-                                for b, c in zip(nums[:-1], nums[1:])):
+        if nums == sorted(set(nums)) and \
+                all(c - b <= d for b, c in zip(nums[:-1], nums[1:])):
             cnt += 1
     return cnt
 
