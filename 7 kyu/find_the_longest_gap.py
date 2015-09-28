be587735011a422ef90c2e6ad0379dcdd7f710ac
G <the-zebulan@users.noreply.github.com>
2015-09-28 17:10:17 -0600

    Added 'find_the_longest_gap'

---
+++ 7 kyu/find_the_longest_gap.py
@@ -0,0 +1,12 @@
+from re import compile, findall
+
+REGEX = compile(r'(?=1(0+)1)')
+
+
+def gap(num):
+    return max(len(a) for a in findall(REGEX, bin(num)) + [''])
+
+assert gap(9) == 2
+assert gap(529) == 4
+assert gap(20) == 1
+assert gap(15) == 0
