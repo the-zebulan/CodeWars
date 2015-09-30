9bb20e3bbade709242f7cd9be0061f854424cbbf
G <the-zebulan@users.noreply.github.com>
2015-09-29 22:01:03 -0600

    Minor refactor, replaced lambda with str.lower

--- Beta/capitals_first.py
+++ Beta/capitals_first.py
@@ -1,6 +1,6 @@
 def capitals_first(string):
     return ' '.join(sorted((a for a in string.split() if a[0].isalpha()),
-                           key=lambda b: b[0].islower()))
+                           key=str.islower))
 
 assert capitals_first('hey You, Sort me Already!') \
     == 'You, Sort Already! hey me'
