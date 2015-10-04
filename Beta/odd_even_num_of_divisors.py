ee99e434097272561dc0b95e6fbe4575366a0797
G <the-zebulan@users.noreply.github.com>
2015-10-03 20:51:01 -0600

    Added 'odd_even_num_of_divisors'

---
+++ Beta/odd_even_num_of_divisors.py
@@ -0,0 +1,17 @@
+def oddity(n):
+    return 'even' if n ** 0.5 % 1 else 'odd'
+
+
+# def oddity(n):
+#     result = 0
+#     for a in xrange(1, int(n ** 0.5 + 1)):
+#         if n % a == 0:
+#             result += 1
+#             if not a == n / a:
+#                 result += 1
+#     return 'odd' if result % 2 else 'even'
+
+assert oddity(1) == 'odd'
+assert oddity(5) == 'even'
+assert oddity(16) == 'odd'
+assert oddity(100) == 'odd'
