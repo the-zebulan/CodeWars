03950baef4ae5c16c70d069e08d89d238f6176be
G <the-zebulan@users.noreply.github.com>
2015-10-06 12:40:58 -0600

    Minor refactor, {:.0f} instead of int()

--- 7 kyu/always_perfect.py
+++ 7 kyu/always_perfect.py
@@ -8,7 +8,7 @@ def check_root(string):
     try:
         total = reduce(lambda a, b: a * b, (int(c) for c in numbers)) + 1
         f, i = modf(total ** 0.5)  # fractional, integer
-        return 'not consecutive' if f else '{}, {}'.format(total, int(i))
+        return 'not consecutive' if f else '{}, {:.0f}'.format(total, i)
     except ValueError:
         return 'incorrect input'
 
