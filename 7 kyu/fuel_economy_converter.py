ae556b6b8301ad808a4c4adb0d849cf2cd1675c2
G <the-zebulan@users.noreply.github.com>
2015-10-29 15:56:31 -0600

    PEP8 - missing & extra whitespace

--- 7 kyu/fuel_economy_converter.py
+++ 7 kyu/fuel_economy_converter.py
@@ -6,9 +6,10 @@ def mpg2lp100km(mpg):
     """ mpg_to_litres_per_100km == PEP8 """
     return round(LITRES_PER_GALLON / ((mpg * KMS_PER_MILE) / 100), 2)
 
+
 def lp100km2mpg(lp100km):
     """ litres_per_100_km_to_mpg == PEP8 """
-    return round((100 / KMS_PER_MILE) / (lp100km / LITRES_PER_GALLON) , 2)
+    return round((100 / KMS_PER_MILE) / (lp100km / LITRES_PER_GALLON), 2)
 
 assert mpg2lp100km(42) == 5.6
 assert lp100km2mpg(9) == 26.13
