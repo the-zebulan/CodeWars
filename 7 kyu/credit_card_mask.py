bb565960c1c6006479a3d5a4386010841b529efd
G <the-zebulan@users.noreply.github.com>
2015-10-29 15:33:44 -0600

    PEP8 - convert lambda to def

--- 7 kyu/credit_card_mask.py
+++ 7 kyu/credit_card_mask.py
@@ -1,4 +1,5 @@
-maskify = lambda cc: '{:#>{}}'.format(cc[-4:], len(cc))
+def maskify(cc):
+    return '{:#>{}}'.format(cc[-4:], len(cc))
 
 assert maskify('4556364607935616') == '############5616'
 assert maskify('64607935616') == '#######5616'
