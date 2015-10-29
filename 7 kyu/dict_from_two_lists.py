97f65d20b89ca5849ed113db9fa29c6f50137ad6
G <the-zebulan@users.noreply.github.com>
2015-10-29 15:34:58 -0600

    PEP8 - line too long, function name lowercase comment

--- 7 kyu/dict_from_two_lists.py
+++ 7 kyu/dict_from_two_lists.py
@@ -2,9 +2,11 @@ from itertools import izip_longest
 
 
 def createDict(keys, values):
+    """ create_dict == PEP8 (forced mixedCase by CodeWars) """
     if len(keys) > len(values):
         return dict(izip_longest(keys, values, fillvalue=None))
     return dict(zip(keys, values))
 
-assert createDict(['a', 'b', 'c', 'd'], [1, 2, 3]), {'a': 1, 'b': 2, 'c': 3, 'd': None}
+assert createDict(['a', 'b', 'c', 'd'], [1, 2, 3]), \
+    {'a': 1, 'b': 2, 'c': 3, 'd': None}
 assert createDict(['a', 'b', 'c'], [1, 2, 3, 4]), {'a': 1, 'b': 2, 'c': 3}
