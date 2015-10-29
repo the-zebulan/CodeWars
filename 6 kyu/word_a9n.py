7339e99d4b89ccd986741083801384ca7a25f3e1
G <the-zebulan@users.noreply.github.com>
2015-10-29 15:53:22 -0600

    PEP8 - line too long

--- 6 kyu/word_a9n.py
+++ 6 kyu/word_a9n.py
@@ -13,7 +13,9 @@ def abbreviate(string):
     # return ''.join(a if len(a) < 4 else OUTPUT(a[0], len(a) - 2, a[-1])
     #                for a in split(r'(\W+)', string))
 
-assert abbreviate('balloon: double-barreled, sat\'mat: sits, a-on, mat\'the, ') == 'b5n: d4e-b6d, sat\'mat: s2s, a-on, mat\'the, '
+assert abbreviate(
+    'balloon: double-barreled, sat\'mat: sits, a-on, mat\'the, ') \
+       == 'b5n: d4e-b6d, sat\'mat: s2s, a-on, mat\'the, '
 assert abbreviate("internationalization") == "i18n"
 assert abbreviate("accessibility") == "a11y"
 assert abbreviate("Accessibility") == "A11y"
