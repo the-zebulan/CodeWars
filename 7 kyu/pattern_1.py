9e602b3446fd19720bd32559b49aecba367dc941
G <the-zebulan@users.noreply.github.com>
2015-10-29 20:05:31 -0600

    PEP8 - do not assign a lambda expression, use a def

--- 7 kyu/pattern_1.py
+++ 7 kyu/pattern_1.py
@@ -1,4 +1,5 @@
-pattern = lambda n: '\n'.join(str(a) * a for a in xrange(1, n + 1))
+def pattern(n):
+    return '\n'.join(str(a) * a for a in xrange(1, n + 1))
 
 assert pattern(1) == '1'
 assert pattern(2) == '1\n22'
