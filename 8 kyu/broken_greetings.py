6d79db8f66c57d8777696f67ecdb1c01fe63d673
G <the-zebulan@users.noreply.github.com>
2015-10-29 20:25:59 -0600

    PEP8 - indentation is not a multiple of four

--- 8 kyu/broken_greetings.py
+++ 8 kyu/broken_greetings.py
@@ -1,9 +1,9 @@
 class Person:
-  def __init__(self, name):
-    self.name = name
+    def __init__(self, name):
+        self.name = name
 
-  def greet(self, other_name):
-    return "Hi {0}, my name is {1}".format(other_name, self.name)
+    def greet(self, other_name):
+        return "Hi {}, my name is {}".format(other_name, self.name)
 
 me = Person('Z')
 assert me.greet('Jim') == 'Hi Jim, my name is Z'
