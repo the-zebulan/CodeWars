ea2f2e8218ae85b8e14ea7c7e1dddd5254425ea4
G <the-zebulan@users.noreply.github.com>
2015-10-29 20:26:54 -0600

    PEP8 - line too long

--- 8 kyu/bug_fixing_6.py
+++ 8 kyu/bug_fixing_6.py
@@ -1,7 +1,8 @@
 def eval_object(v):
     return {'+': v['a'] + v['b'], '-': v['a'] - v['b'],
             '/': v['a'] / v['b'], '*': v['a'] * v['b'],
-            '%': v['a'] % v['b'], '**': v['a'] ** v['b']}.get(v['operation'], 1)
+            '%': v['a'] % v['b'], '**': v['a'] ** v['b']}.\
+        get(v['operation'], 1)
 
 assert eval_object({'a': 1, 'b': 1, 'operation': '+'}) == 2
 assert eval_object({'a': 1, 'b': 1, 'operation': '-'}) == 0
