2ffc9e3b2fc97e4db8f2a8ae5145708b362501ef
G <the-zebulan@users.noreply.github.com>
2015-10-29 20:36:16 -0600

    Typo - extra comma

--- Beta/friend_or_foe.py
+++ Beta/friend_or_foe.py
@@ -1,4 +1,5 @@
 def friend(x):
     return [a for a in x if len(a) == 4]
 
-assert friend(["Ryan", "Kieran", "Mark",]) == ["Ryan", "Mark"]
+
+assert friend(['Ryan', 'Kieran', 'Mark']) == ['Ryan', 'Mark']
