8fb65b2d8dfdbe515112ac9169107beeda0d95b2
G <the-zebulan@users.noreply.github.com>
2015-10-29 20:21:42 -0600

    PEP8 - line too long

--- 7 kyu/the_old_switcheroo.py
+++ 7 kyu/the_old_switcheroo.py
@@ -6,5 +6,6 @@ def vowel_2_index(string):
                    for i, a in enumerate(string, start=1))
 
 assert vowel_2_index('this is my string') == 'th3s 6s my str15ng'
-assert vowel_2_index('Codewars is the best site in the world') == 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld'
+assert vowel_2_index('Codewars is the best site in the world') \
+       == 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld'
 assert vowel_2_index('') == ''
