73bca29e97f66dd5df3c003f96366db9bc1e7285
G <the-zebulan@users.noreply.github.com>
2015-10-29 20:28:20 -0600

    PEP8 - indentation is not a multiple of four

--- 8 kyu/even_or_odd.py
+++ 8 kyu/even_or_odd.py
@@ -1,5 +1,6 @@
 def even_or_odd(number):
-  return 'Even' if number % 2 == 0 else 'Odd'
+    return ('Even', 'Odd')[number % 2]
+    # return 'Even' if number % 2 == 0 else 'Odd'
 
 assert even_or_odd(2) == 'Even'
 assert even_or_odd(11) == 'Odd'
