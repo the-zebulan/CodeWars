5d015f5b2918122efdf68a1ac52fbfaf3f7a3bc5
G <the-zebulan@users.noreply.github.com>
2015-10-29 20:01:11 -0600

    PEP8 - missing whitespace, convert lambda to def

--- 7 kyu/monotone_travel.py
+++ 7 kyu/monotone_travel.py
@@ -1,7 +1,8 @@
-is_monotone = lambda heights: sorted(heights) == heights
+def is_monotone(heights):
+    return sorted(heights) == heights
 
 assert is_monotone(range(1, 11)) is True
-assert is_monotone([5,5,5,5,5,5,5]) is True
+assert is_monotone([5, 5, 5, 5, 5, 5, 5]) is True
 assert is_monotone([]) is True
 assert is_monotone([1]) is True
 assert is_monotone(range(5, 0, -1)) is False
