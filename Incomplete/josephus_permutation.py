db5fb37ec412a12c98484bf29d497203ffb70115
G <the-zebulan@users.noreply.github.com>
2015-10-29 21:24:25 -0600

    Deleted incomplete problems

--- Incomplete/josephus_permutation.py
+++
@@ -1,9 +0,0 @@
-def josephus(n, k):
-    n = range(1, n + 1)
-    print n
-    result = []
-    while n:
-        pass
-        break
-
-josephus(7, 3)
--- Incomplete/sum_2_total.py
+++
@@ -1,5 +0,0 @@
-def total(numbers):
-    pass
-
-# total([-1,-1,-1])  # == -4
-# total([1,2,3,4])  # == 20
