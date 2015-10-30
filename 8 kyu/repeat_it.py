6f75e5add47329a0d4816cca20022177ec04c908
G <the-zebulan@users.noreply.github.com>
2015-10-29 20:31:23 -0600

    PEP8 - missing whitespace, line too long

--- 8 kyu/repeat_it.py
+++ 8 kyu/repeat_it.py
@@ -1,5 +1,7 @@
 def repeat_it(string, n):
     return string * n if isinstance(string, str) else 'Not a string'
 
-assert repeat_it("*",3) == "***"
-assert repeat_it("Hello",11) == "HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello"
+
+assert repeat_it('*', 3) == '***'
+assert repeat_it('Hello', 11) \
+    == 'HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello'
