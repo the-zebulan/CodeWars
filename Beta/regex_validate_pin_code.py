be14f2ec8e7cfd663edaf9d570d36699270994ad
G <the-zebulan@users.noreply.github.com>
2015-10-29 21:19:18 -0600

    Minor function name change

--- Beta/regex_validate_pin_code.py
+++ Beta/regex_validate_pin_code.py
@@ -1,13 +1,12 @@
 from re import match
 
 
-def validate_PIN(pin):
-    """ validate_pin == PEP8, forced mixedCase by CodeWars """
+def validate_pin(pin):
     return bool(match(r'(\d{4}|\d{6})$', pin))
 
-assert validate_PIN('') is False
-assert validate_PIN('1') is False
-assert validate_PIN('1234567') is False
-assert validate_PIN('-1234') is False
-assert validate_PIN('1111') is True
-assert validate_PIN('123456') is True
+assert validate_pin('') is False
+assert validate_pin('1') is False
+assert validate_pin('1234567') is False
+assert validate_pin('-1234') is False
+assert validate_pin('1111') is True
+assert validate_pin('123456') is True
