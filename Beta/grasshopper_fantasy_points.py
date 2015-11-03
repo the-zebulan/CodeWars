b0af33dc8cb57613ced84ed271727c32aa787a8b
G <the-zebulan@users.noreply.github.com>
2015-11-03 13:35:31 -0700

    Added 'grasshopper_fantasy_points'

---
+++ Beta/grasshopper_fantasy_points.py
@@ -0,0 +1,7 @@
+assists = crosses = 3
+chancesCreated = goals = 4
+shotsOnTarget = 5
+successfulDribbles = 10
+points = 89
+# points = goals * 9 + assists * 6 + chancesCreated * 3 + \
+#          shotsOnTarget * 2 + crosses + successfulDribbles
