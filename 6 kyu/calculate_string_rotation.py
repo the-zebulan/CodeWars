b5c44601eae3a394f068f166b8740c9f3ec2e078
G <the-zebulan@users.noreply.github.com>
2015-11-05 14:53:55 -0700

    Added 'calculate_string_rotation'

---
+++ 6 kyu/calculate_string_rotation.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+
+def shifted_diff(first, second):
+    if not Counter(first) == Counter(second):
+        return -1
+    for i, a in enumerate(xrange(len(first), -1, -1)):
+        if second == '{}{}'.format(first[a:], first[:a]):
+            return i
+    return -1
+
+
+assert shifted_diff("coffee", "eecoff") == 2
+assert shifted_diff("eecoff", "coffee") == 4
+assert shifted_diff("moose", "Moose") == -1
+assert shifted_diff("isn't", "'tisn") == 2
+assert shifted_diff("Esham", "Esham") == 0
+assert shifted_diff("dog", "god") == -1
+assert shifted_diff("yoyo", "oyoyoy") == -1
