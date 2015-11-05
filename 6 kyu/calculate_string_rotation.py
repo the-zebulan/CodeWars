fe4280a56239e6ecd156a93010ba2bb43f082571
G <the-zebulan@users.noreply.github.com>
2015-11-05 15:01:44 -0700

    Comment with another (nicer) solution from CodeWars

--- 6 kyu/calculate_string_rotation.py
+++ 6 kyu/calculate_string_rotation.py
@@ -2,6 +2,8 @@ from collections import Counter
 
 
 def shifted_diff(first, second):
+    # pivek303 - one line solution from CodeWars
+    # return (second + second).find(first) if len(first) == len(second) else - 1
     if not Counter(first) == Counter(second):
         return -1
     for i, a in enumerate(xrange(len(first), -1, -1)):
