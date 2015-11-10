9e9bb828c67073b3bf50c2589ffdeba9f49ff199
G <the-zebulan@users.noreply.github.com>
2015-11-09 22:46:00 -0700

    Variable renamed, more descriptive

--- Beta/time_for_validation.py
+++ Beta/time_for_validation.py
@@ -5,17 +5,17 @@ REGEX_12 = compile(r'[0-1]*\d:[0-5]\d(am|pm)', IGNORECASE)
 REGEX_24 = compile(r'[0-2]\d:[0-5]\d$')
 
 
-def convert_time(output_format, time):
+def convert_time(output_format, input_time):
     if output_format not in ('12', '24'):
         return False
-    elif match(REGEX_12, time):
+    elif match(REGEX_12, input_time):
         if output_format == '24':
-            return strftime('%H:%M', strptime(time, '%I:%M%p'))
-        return time
-    elif match(REGEX_24, time):
+            return strftime('%H:%M', strptime(input_time, '%I:%M%p'))
+        return input_time
+    elif match(REGEX_24, input_time):
         if output_format == '12':
-            return strftime('%I:%M%p', strptime(time, '%H:%M'))
-        return time
+            return strftime('%I:%M%p', strptime(input_time, '%H:%M'))
+        return input_time
     return False
 
 
