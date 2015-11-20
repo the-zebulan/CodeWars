0c83a8418869fdded957ac8f5fef4e5d57025d2e
G <the-zebulan@users.noreply.github.com>
2015-11-20 10:37:49 -0700

    Refactored without using collections.Counter

--- 5 kyu/where_my_anagrams_at.py
+++ 5 kyu/where_my_anagrams_at.py
@@ -1,9 +1,14 @@
-from collections import Counter
+# from collections import Counter
+#
+#
+# def anagrams(word, words):
+#     word_cnt = Counter(word)
+#     return [a for a in words if Counter(a) == word_cnt]
 
 
 def anagrams(word, words):
-    word_cnt = Counter(word)
-    return [a for a in words if Counter(a) == word_cnt]
+    word_sorted = sorted(word)
+    return [a for a in words if sorted(a) == word_sorted]
 
 
 assert anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) == ['aabb', 'bbaa']
