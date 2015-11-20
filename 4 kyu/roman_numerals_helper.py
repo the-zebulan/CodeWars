594376479a9aba88a0b3f97923bb4802662c18e7
G <the-zebulan@users.noreply.github.com>
2015-11-19 21:15:23 -0700

    Added 'roman_numerals_helper'

---
+++ 4 kyu/roman_numerals_helper.py
@@ -0,0 +1,26 @@
+from itertools import groupby, izip_longest
+
+FROM_ROMAN = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+TO_ROMAN = ((1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+            (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'),
+            (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I'))
+
+
+class RomanNumerals(object):
+    @staticmethod
+    def to_roman(n):
+        result = []
+        for num, char in TO_ROMAN:
+            q, n = divmod(n, num)
+            result.append(q * char)
+        return ''.join(result)
+
+    @staticmethod
+    def from_roman(roman):
+        pairs = [sum(g) for _, g in groupby(FROM_ROMAN[a] for a in roman)]
+        return sum(a + b if a > b else b - a for a, b in
+                   izip_longest(pairs[::2], pairs[1::2], fillvalue=0))
+
+
+assert RomanNumerals.to_roman(1000) == 'M'
+assert RomanNumerals.from_roman('M') == 1000
