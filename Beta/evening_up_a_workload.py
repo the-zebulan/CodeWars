f6230d5f4731be6bc8838827e2aabdf232523954
G <the-zebulan@users.noreply.github.com>
2015-11-22 14:50:56 -0700

    2.0, inspired by another CodeWars solution

--- Beta/evening_up_a_workload.py
+++ Beta/evening_up_a_workload.py
@@ -1,11 +1,23 @@
+# def split_workload(workload):
+#     left_sum = 0
+#     right_sum = sum(workload)
+#     minimum = (abs(right_sum), 0)
+#     for i, a in enumerate(workload, 1):
+#         left_sum += a
+#         right_sum -= a
+#         current = (abs(right_sum - left_sum), i)
+#         if current < minimum:
+#             minimum = current
+#     return (None, None) if minimum == (0, 0) else minimum[::-1]
+
+
 def split_workload(workload):
-    left_sum = 0
-    right_sum = sum(workload)
-    minimum = (abs(right_sum), 0)
-    for i, a in enumerate(workload, 1):
-        left_sum += a
-        right_sum -= a
-        current = (abs(right_sum - left_sum), i)
+    """ Fork of CrazyMerlyn's solution from CodeWars """
+    diff = sum(workload)
+    minimum = abs(diff), 0
+    for i, work in enumerate(workload, 1):
+        diff -= 2 * work
+        current = abs(diff), i
         if current < minimum:
             minimum = current
     return (None, None) if minimum == (0, 0) else minimum[::-1]
