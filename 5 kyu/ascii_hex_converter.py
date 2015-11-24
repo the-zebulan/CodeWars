6c1d0c5e72fcf39fb1eda9e5c869edc35ee5198e
G <the-zebulan@users.noreply.github.com>
2015-11-24 15:21:08 -0700

    Added 'ascii_hex_converter'

---
+++ 5 kyu/ascii_hex_converter.py
@@ -0,0 +1,17 @@
+HEX = '{:x}'.format
+
+
+class Converter(object):
+    @staticmethod
+    def to_ascii(s):
+        return ''.join(chr(int(s[a:a + 2], 16)) for a in xrange(0, len(s), 2))
+
+    @staticmethod
+    def to_hex(s):
+        return ''.join(HEX(ord(b)) for b in s)
+
+
+assert Converter.to_hex("Look mom, no hands") \
+    == "4c6f6f6b206d6f6d2c206e6f2068616e6473"
+assert Converter.to_ascii("4c6f6f6b206d6f6d2c206e6f2068616e6473") \
+    == "Look mom, no hands"
