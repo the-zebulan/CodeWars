912674686131d70982b17dc9734e56b430f1f644
G <the-zebulan@users.noreply.github.com>
2015-11-24 15:23:49 -0700

    Minor refactor using encode/decode (learned from CodeWars)

--- 5 kyu/ascii_hex_converter.py
+++ 5 kyu/ascii_hex_converter.py
@@ -1,14 +1,24 @@
-HEX = '{:x}'.format
+# HEX = '{:x}'.format
+#
+#
+# class Converter(object):
+#     @staticmethod
+#     def to_ascii(s):
+#         return ''.join(chr(int(s[a:a + 2], 16)) for a in xrange(0, len(s), 2))
+#
+#     @staticmethod
+#     def to_hex(s):
+#         return ''.join(HEX(ord(b)) for b in s)
 
 
 class Converter(object):
     @staticmethod
     def to_ascii(s):
-        return ''.join(chr(int(s[a:a + 2], 16)) for a in xrange(0, len(s), 2))
+        return s.decode('hex')
 
     @staticmethod
     def to_hex(s):
-        return ''.join(HEX(ord(b)) for b in s)
+        return s.encode('hex')
 
 
 assert Converter.to_hex("Look mom, no hands") \
