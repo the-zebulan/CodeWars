c25fb0cbdde2ba9c8c1fbef63932e8a96c00cc09
G <the-zebulan@users.noreply.github.com>
2015-11-24 14:23:04 -0700

    Added 'slope_of_a_line'

---
+++ 7 kyu/slope_of_a_line.py
@@ -0,0 +1,13 @@
+def getSlope(p1, p2):
+    """ get_slope == PEP8 (forced mixedCase by CodeWars) """
+    (x, y), (x2, y2) = p1, p2
+    try:
+        return (y - y2) / (x - x2)
+    except ZeroDivisionError:
+        pass
+
+
+assert getSlope([1, 1], [2, 2]) == 1
+assert getSlope([11, 1], [1, 11]) == -1
+assert getSlope([1, 1], [1, 2]) is None
+assert getSlope([1, 1], [1, 1]) is None
