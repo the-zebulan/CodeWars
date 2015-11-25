a50f98eff94a589287f0207f3e29c964bf2c4f7f
G <the-zebulan@users.noreply.github.com>
2015-11-25 12:36:04 -0700

    Refactored thanks to a CodeWars solution, formula instead

--- 7 kyu/unique_pairs.py
+++ 7 kyu/unique_pairs.py
@@ -1,9 +1,6 @@
-from itertools import combinations
-
-
 def projectPartners(n):
     """ project_partners == PEP8 (forced mixedCase by CodeWars) """
-    return sum(1 for _ in combinations('0' * n, 2))
+    return n * (n - 1) / 2
 
 
 assert projectPartners(2) == 1
