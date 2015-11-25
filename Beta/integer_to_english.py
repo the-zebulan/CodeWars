837589d9e577527eefccd2d0791c1fccb29230dd
G <the-zebulan@users.noreply.github.com>
2015-11-24 18:47:05 -0700

    Added 'integer_to_english'

---
+++ Beta/integer_to_english.py
@@ -0,0 +1,30 @@
+WORDS = ((10 ** 24, 'septillion'), (10 ** 21, 'sextillion'),
+         (10 ** 18, 'quintillion'), (10 ** 15, 'quadrillion'),
+         (10 ** 12, 'trillion'), (10 ** 9, 'billion'), (10 ** 6, 'million'),
+         (10 ** 3, 'thousand'), (10 ** 2, 'hundred'), (90, 'ninety'),
+         (80, 'eighty'), (70, 'seventy'), (60, 'sixty'), (50, 'fifty'),
+         (40, 'forty'), (30, 'thirty'), (20, 'twenty'), (19, 'nineteen'),
+         (18, 'eighteen'), (17, 'seventeen'), (16, 'sixteen'), (15, 'fifteen'),
+         (14, 'fourteen'), (13, 'thirteen'), (12, 'twelve'), (11, 'eleven'),
+         (10, 'ten'), (9, 'nine'), (8, 'eight'), (7, 'seven'), (6, 'six'),
+         (5, 'five'), (4, 'four'), (3, 'three'), (2, 'two'), (1, 'one'))
+
+
+def int_to_english(num):
+    result = []
+    for word_value, word_name in WORDS:
+        q, num = divmod(num, word_value)
+        if q:
+            if word_value >= 100:
+                result.append(int_to_english(q))
+            result.append(word_name)
+        if not num:
+            return ' '.join(result)
+
+
+assert int_to_english(1) == 'one'
+assert int_to_english(25) == 'twenty five'
+assert int_to_english(161) == 'one hundred sixty one'
+assert int_to_english(25161045656) \
+      == 'twenty five billion one hundred sixty one million forty ' \
+         'five thousand six hundred fifty six'
