a61c9908f9fd7c38ebe7e276cc04516a0636fff9
G <the-zebulan@users.noreply.github.com>
2015-11-25 14:18:05 -0700

    Added 'tic_tac_toe_checker'

---
+++ 5 kyu/tic_tac_toe_checker.py
@@ -0,0 +1,31 @@
+from itertools import chain, izip
+
+
+def isSolved(board):
+    """ is_solved == PEP8 (forced mixedCase by CodeWars) """
+    cats_game = True
+    diagonals = [[], []]
+    for i, row in enumerate(board):
+        current = set(row)
+        if current == {1}:
+            return 1
+        elif current == {2}:
+            return 2
+        if cats_game and 0 in current:
+            cats_game = False
+        diagonals[0].append(row[i])      # nw -> se
+        diagonals[1].append(row[2 - i])  # ne -> sw
+
+    for col in chain(izip(*board), diagonals):
+        current = set(col)
+        if current == {1}:
+            return 1
+        elif current == {2}:
+            return 2
+        if cats_game and 0 in current:
+            cats_game = False
+    return 0 if cats_game else -1
+
+
+assert isSolved([[0, 0, 1], [0, 1, 2], [2, 1, 0]]) == -1
+assert isSolved([[1, 2, 1], [1, 1, 2], [2, 1, 2]]) == 0
