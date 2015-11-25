779e465addff6e2e6ec7eabc1635ceab86b3b141
G <the-zebulan@users.noreply.github.com>
2015-11-24 23:21:14 -0700

    Example of using sum for 1 level of sublists

--- Beta/flattening_lists.py
+++ Beta/flattening_lists.py
@@ -4,3 +4,6 @@ def flatten(lst):
 
 assert flatten([[1, 2], [3, 4]]) == [1, 2, 3, 4]
 assert flatten([[1], [2], [3], [4]]) == [1, 2, 3, 4]
+
+print sum([[1], [2], [3], [4], [5]], [])  # only works for one level nested
+print sum([[1], [2], [3], [4], [[5]]], [])  # doesn't flatten the 5
