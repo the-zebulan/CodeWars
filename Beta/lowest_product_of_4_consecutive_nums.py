60eccaefeec85738927759a8fc3f5b058e905186
G <the-zebulan@users.noreply.github.com>
2015-11-29 19:31:55 -0700

    Added 'lowest_product_of_4_consecutive_nums'

---
+++ Beta/lowest_product_of_4_consecutive_nums.py
@@ -0,0 +1,41 @@
+def lowest_product(nums):
+    int_nums = []
+    length = 0
+    for i, a in enumerate(nums, 1):
+        int_nums.append(int(a))
+        length += 1
+    return 'Number is too small' if length < 4 else \
+        min(reduce(lambda x, y: x * y, int_nums[b:b + 4])
+            for b in xrange(length - 3))
+
+
+assert lowest_product("123456789") == 24
+assert lowest_product("2345611117899") == 1
+assert lowest_product("2305611117899") == 0
+assert lowest_product("333") == "Number is too small"
+assert lowest_product("1234111")  # == 4  # ,"Numbers should be consecutives"
+
+
+# # second try:
+# def lowest_product(nums):
+#     int_nums = []
+#     for i, a in enumerate(nums, 1):
+#         int_nums.append(int(a))
+#     else:
+#         if i < 4:
+#             return 'Number is too small'
+#     cnt = 0
+#     minimum = None
+#     total = 1
+#     for i, b in enumerate(int_nums):
+#         total *= b
+#         if i >= 3:
+#             try:
+#                 total /= int_nums[cnt]
+#                 print total, int_nums[cnt], cnt
+#             except ZeroDivisionError:
+#                 pass
+#             cnt += 1
+#             if minimum is None or total < minimum:
+#                 minimum = total
+#     return minimum
