630c2383b0477f80ce1f87062810bc0a1e749a02
G <the-zebulan@users.noreply.github.com>
2015-12-15 22:51:57 -0700

    Added 'maximum_length_difference'

---
+++ 7 kyu/maximum_length_difference.py
@@ -0,0 +1,17 @@
+def min_max(lst):
+    a = sorted(len(b) for b in lst)
+    return a[0], a[-1]
+
+
+def mxdiflg(a1, a2):
+    if not a1 or not a2:
+        return -1
+    a, b = min_max(a1)
+    c, d = min_max(a2)
+    return max(abs(d - a), abs(b - c))
+
+
+s1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt",
+      "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+s2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+assert mxdiflg(s1, s2) == 13
