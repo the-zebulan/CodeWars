2dc1f42aa8b9dd8b3b3286cbc9f834e2e7c87549
G <the-zebulan@users.noreply.github.com>
2015-12-17 22:56:44 -0700

    Added 'reversing_fun'

---
+++ Beta/reversing_fun.py
@@ -0,0 +1,12 @@
+from itertools import izip_longest
+
+
+def ReverseFun(n):
+    """ reverse_fun == PEP8 (forced CamelCase by CodeWars) """
+    half = len(n) / 2
+    return ''.join(''.join(a) for a in
+                   izip_longest(reversed(n[half:]), n[:half], fillvalue=''))
+
+
+assert ReverseFun('012345') == '504132'
+assert ReverseFun('jointhedarkside') == 'ejdoiisnktrhaed'
