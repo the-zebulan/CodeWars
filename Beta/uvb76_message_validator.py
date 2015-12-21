271438adabaa9553a1dcf6d7cda08daca6a7e5ec
G <the-zebulan@users.noreply.github.com>
2015-12-21 14:26:40 -0700

    Kata was updated on CodeWars (added regex EOL anchor)

--- Beta/uvb76_message_validator.py
+++ Beta/uvb76_message_validator.py
@@ -1,6 +1,6 @@
 from re import compile, match
 
-REGEX = compile(r'MDZHB \d{2} \d{3} [A-Z]+ \d{2} \d{2} \d{2} \d{2}')
+REGEX = compile(r'MDZHB \d{2} \d{3} [A-Z]+ \d{2} \d{2} \d{2} \d{2}$')
 
 
 def validate(message):
