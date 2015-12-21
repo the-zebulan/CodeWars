62bf054d30db2cda49bde387b4b6d5a48b30d7c8
G <the-zebulan@users.noreply.github.com>
2015-12-21 14:08:03 -0700

    Typo fixed on CodeWars (Goldem -> Golden)

--- 7 kyu/mobile_operator_detector.py
+++ 7 kyu/mobile_operator_detector.py
@@ -1,5 +1,5 @@
 OPERATORS = {
-    '039': 'Goldem Telecom', '050': 'MTS', '063': 'Life:)', '066': 'MTS',
+    '039': 'Golden Telecom', '050': 'MTS', '063': 'Life:)', '066': 'MTS',
     '067': 'Kyivstar', '068': 'Beeline', '093': 'Life:)', '095': 'MTS',
     '096': 'Kyivstar', '097': 'Kyivstar', '098': 'Kyivstar', '099': 'MTS'}
 
