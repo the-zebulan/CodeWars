660c9a955fb8ae8af871c34333c6ff2b44ed339c
G <the-zebulan@users.noreply.github.com>
2016-01-08 00:07:05 -0700

    Added 'finish_guess_the_number_game'

--- 8 kyu/finish_guess_the_number_game.py
+++ 8 kyu/finish_guess_the_number_game.py
@@ -4,4 +4,21 @@ class Guesser:
         self.lives = lives
 
     def guess(self, n):
+        if self.lives == 0:
+            raise Exception('Omae wa mo shindeiru')
+        elif n == self.number:
+            return True
+        self.lives -= 1
         return False
+
+
+guesser = Guesser(10, 2)
+guesser.guess(10)
+guesser.guess(10)
+guesser.guess(10)
+guesser.guess(10)
+assert guesser.guess(10) is True
+
+guesser = Guesser(10, 2)
+guesser.guess(1)
+assert guesser.guess(1) is False
