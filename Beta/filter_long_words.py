2c5d1ac98e269ed573f71fe5079f0e0c6af66b94
G <the-zebulan@users.noreply.github.com>
2016-01-18 15:27:29 -0700

    Variable name fix, 'long' is a built-in name

--- Beta/filter_long_words.py
+++ Beta/filter_long_words.py
@@ -1,5 +1,5 @@
-def filter_long_words(sentence, long):
-    return filter(lambda a: len(a) > long, sentence.split())
+def filter_long_words(sentence, n):
+    return filter(lambda a: len(a) > n, sentence.split())
 
 
 assert filter_long_words('The quick brown fox jumps over the lazy dog', 4) \
