eda41dd6cb8981f53e83004204ba75094bd058a2
G <the-zebulan@users.noreply.github.com>
2016-01-19 11:26:05 -0700

    Removed variable name, only used once

--- Beta/classy_instance_1.py
+++ Beta/classy_instance_1.py
@@ -1,12 +1,11 @@
 def show_me(instname):
-    name = instname.__class__.__name__
     attrs = sorted(instname.__dict__.iterkeys())
     if len(attrs) == 1:
         attrs = attrs[0]
     else:
         attrs = '{} and {}'.format(', '.join(attrs[:-1]), attrs[-1])
     return 'Hi, I\'m one of those {}s! Have a look at my {}.'\
-        .format(name, attrs)
+        .format(instname.__class__.__name__, attrs)
 
 
 # Kata solution is the function above
