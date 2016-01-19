a9d1c4c0ddca1660e4418444522992c1c02972dd
G <the-zebulan@users.noreply.github.com>
2016-01-19 11:22:35 -0700

    Added 'classy_instance_1'

---
+++ Beta/classy_instance_1.py
@@ -0,0 +1,31 @@
+def show_me(instname):
+    name = instname.__class__.__name__
+    attrs = sorted(instname.__dict__.iterkeys())
+    if len(attrs) == 1:
+        attrs = attrs[0]
+    else:
+        attrs = '{} and {}'.format(', '.join(attrs[:-1]), attrs[-1])
+    return 'Hi, I\'m one of those {}s! Have a look at my {}.'\
+        .format(name, attrs)
+
+
+# Kata solution is the function above
+# Below this line is just for testing
+class Vehicle:
+    def __init__(self, seats, wheels, engine):
+        self.seats = seats
+        self.wheels = wheels
+        self.engine = engine
+
+
+class Planet:
+    def __init__(self, moon):
+        self.moon = moon
+
+
+porsche = Vehicle(2, 4, 'Gas')
+assert show_me(porsche) == "Hi, I'm one of those Vehicles! Have a look " \
+                          "at my engine, seats and wheels."
+
+earth = Planet('moon')
+assert show_me(earth) == "Hi, I'm one of those Planets! Have a look at my moon."
