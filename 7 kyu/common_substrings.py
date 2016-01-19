c40d820fcee5a922f3cb9f77f12dfbd25b33e6d8
G <the-zebulan@users.noreply.github.com>
2016-01-18 22:40:26 -0700

    Added 'common_substrings'

--- 7 kyu/common_substrings.py
+++ 7 kyu/common_substrings.py
@@ -1,23 +1,30 @@
-def substring_test(str1, str2):
-    pass
+def substring_test(*args):
+    shorter, longer = sorted((a.lower() for a in args), key=len)
+    length = len(shorter) + 1
+    for b in xrange(2, length):
+        for c in xrange(length - b):
+            if longer.find(shorter[c:c + b]) != -1:
+                return True
+    return False
 
 
-assert substring_test("Something", "Home") is True
-assert substring_test("Something", "Fun") is False
-assert substring_test("Something", "") is False
-assert substring_test("", "Something") is False
-assert substring_test("BANANA", "banana") is True
-assert substring_test("test", "lllt") is False
-assert substring_test("", "") is False
-assert substring_test("1234567", "541265") is True
-assert substring_test("supercalifragilisticexpialidocious",
-                      "SoundOfItIsAtrocious") is True
+assert substring_test('Something', 'Home') is True
+assert substring_test('Something', 'Fun') is False
+assert substring_test('Something', '') is False
+assert substring_test('', 'Something') is False
+assert substring_test('BANANA', 'banana') is True
+assert substring_test('test', 'lllt') is False
+assert substring_test('', '') is False
+assert substring_test('1234567', '541265') is True
+assert substring_test('Codewars is sweet!', 'is') is True
+assert substring_test('supercalifragilisticexpialidocious',
+                      'SoundOfItIsAtrocious') is True
 assert substring_test(
-    "LoremipsumdolorsitametconsecteturadipiscingelitAeneannonaliquetligulaut"
-    "placeratorciSuspendissepotentiMorbivolutpatauctoripsumegetaliquamPhasel"
-    "lusidmagnaelitNullamerostellustemporquismolestieaornarevitaediamNullaal"
-    "iquamrisusnonviverrasagittisInlaoreetultricespretiumVestibulumegetnulla"
-    "tinciduntsempersemacrutrumfelisPraesentpurusarcutempusnecvariusidultric"
-    "esaduiPellentesqueultriciesjustolobortisrhoncusdignissimNuncviverracons"
-    "equatblanditUtbibendumatlacusactristiqueAliquamimperdietnuncsempertorto"
-    "refficiturviverra", "thisisalongstringtest") is True
+    'LoremipsumdolorsitametconsecteturadipiscingelitAeneannonaliquetligulaut'
+    'placeratorciSuspendissepotentiMorbivolutpatauctoripsumegetaliquamPhasel'
+    'lusidmagnaelitNullamerostellustemporquismolestieaornarevitaediamNullaal'
+    'iquamrisusnonviverrasagittisInlaoreetultricespretiumVestibulumegetnulla'
+    'tinciduntsempersemacrutrumfelisPraesentpurusarcutempusnecvariusidultric'
+    'esaduiPellentesqueultriciesjustolobortisrhoncusdignissimNuncviverracons'
+    'equatblanditUtbibendumatlacusactristiqueAliquamimperdietnuncsempertorto'
+    'refficiturviverra', 'thisisalongstringtest') is True
