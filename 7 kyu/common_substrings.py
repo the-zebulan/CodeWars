b3ad8c4339c3f8bdc4d561b62dfd38d5f1872449
G <the-zebulan@users.noreply.github.com>
2016-01-18 22:48:31 -0700

    Minor refactor, no need to check past chunk of length 2

--- 7 kyu/common_substrings.py
+++ 7 kyu/common_substrings.py
@@ -1,10 +1,8 @@
 def substring_test(*args):
     shorter, longer = sorted((a.lower() for a in args), key=len)
-    length = len(shorter) + 1
-    for b in xrange(2, length):
-        for c in xrange(length - b):
-            if longer.find(shorter[c:c + b]) != -1:
-                return True
+    for b in xrange(len(shorter) - 1):
+        if longer.find(shorter[b:b + 2]) != -1:
+            return True
     return False
 
 
