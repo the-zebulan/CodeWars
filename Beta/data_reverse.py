a1ee612802650ff5f05c99fc00d3920097f1aacd
G <the-zebulan@users.noreply.github.com>
2016-01-21 22:54:20 -0700

    Added 'data_reverse'

---
+++ Beta/data_reverse.py
@@ -0,0 +1,14 @@
+def DataReverse(data):
+    """ data_reverse == PEP8 """
+    return [b for a in xrange(len(data) - 8, -1, -8) for b in data[a:a + 8]]
+
+
+data1 = [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,
+         1, 0, 1, 0, 1, 0, 1, 0]
+data2 = [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+         1, 1, 1, 1, 1, 1, 1, 1]
+assert DataReverse(data1) == data2
+
+data3 = [0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1]
+data4 = [0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0]
+assert DataReverse(data3) == data4
