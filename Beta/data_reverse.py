fcc991fa3700b629f782d6d2b5751f42ca351508
G <the-zebulan@users.noreply.github.com>
2016-01-22 10:54:19 -0700

    Kata was updated according to PEP8, function name changed

--- Beta/data_reverse.py
+++ Beta/data_reverse.py
@@ -1,5 +1,4 @@
-def DataReverse(data):
-    """ data_reverse == PEP8 """
+def data_reverse(data):
     return [b for a in xrange(len(data) - 8, -1, -8) for b in data[a:a + 8]]
 
 
@@ -7,8 +6,8 @@ data1 = [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,
          1, 0, 1, 0, 1, 0, 1, 0]
 data2 = [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
          1, 1, 1, 1, 1, 1, 1, 1]
-assert DataReverse(data1) == data2
+assert data_reverse(data1) == data2
 
 data3 = [0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1]
 data4 = [0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0]
-assert DataReverse(data3) == data4
+assert data_reverse(data3) == data4
