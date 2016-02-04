7aa27fe6fa3b80ce2060b186adc51b72f2370da3
G <the-zebulan@users.noreply.github.com>
2016-02-03 22:19:08 -0700

    Added another version that accounts for repeated numbers

--- Beta/sum_factorial.py
+++ Beta/sum_factorial.py
@@ -11,3 +11,27 @@ def sum_factorial(lst):
 
 assert sum_factorial([4, 6]) == 744
 assert sum_factorial([5, 4, 1]) == 145
+
+
+# # repeated values would break the solution above but according to
+# # the kata constraints: "Note: Assume that all values in the list are
+# # positive integer values > 0 and each value in the list is unique"
+# #
+# # as an exercise, the code below will solve the problem even
+# # with repeated (and positive) nums
+# from itertools import groupby
+#
+#
+# def sum_factorial(lst):
+#     nums = dict((k, sum(1 for _ in g)) for k, g in groupby(sorted(lst)))
+#     current = 1
+#     total = 0
+#     for a in xrange(1, max(nums) + 1):
+#         current *= a
+#         total += current * nums.get(a, 0)
+#     return total
+#
+#
+# assert sum_factorial([4, 6]) == 744
+# assert sum_factorial([5, 4, 1]) == 145
+# assert sum_factorial([4, 4, 4]) == 72  # <-- repeated
