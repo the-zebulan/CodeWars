55a0062aca6a1ad55670004b2d61eaef37bd4a87
G <the-zebulan@users.noreply.github.com>
2016-02-10 23:51:51 -0700

    Refactored thanks to a solution on CodeWars

--- Beta/greek_sort.py
+++ Beta/greek_sort.py
@@ -1,12 +1,11 @@
-GREEK = {'alpha': 0, 'beta': 1, 'chi': 21, 'delta': 3, 'epsilon': 4, 'eta': 6,
-         'gamma': 2, 'iota': 8, 'kappa': 9, 'lambda': 10, 'mu': 11, 'nu': 12,
-         'omega': 23, 'omicron': 14, 'phi': 20, 'pi': 15, 'psi': 22, 'rho': 16,
-         'sigma': 17, 'tau': 18, 'theta': 7, 'upsilon': 19, 'xi': 13, 'zeta': 5}
+greek_alphabet = ('alpha', 'beta', 'gamma', 'delta', 'epsilon', 'zeta', 'eta',
+                  'theta', 'iota', 'kappa', 'lambda', 'mu', 'nu', 'xi',
+                  'omicron', 'pi', 'rho', 'sigma', 'tau', 'upsilon', 'phi',
+                  'chi', 'psi', 'omega')
 
 
 def greek_comparator(lhs, rhs):
-    return ((-1, 1)[GREEK[lhs] > GREEK[rhs]], 0)[lhs == rhs]
-    # return 0 if lhs == rhs else -1 if GREEK[lhs] < GREEK[rhs] else 1
+    return cmp(greek_alphabet.index(lhs), greek_alphabet.index(rhs))
 
 
 assert greek_comparator('alpha', 'beta') == -1
