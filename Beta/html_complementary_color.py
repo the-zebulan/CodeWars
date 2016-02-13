e272b8edf3941417694c2890a546ff75ee253e42
G <the-zebulan@users.noreply.github.com>
2016-02-13 14:02:54 -0700

    Refactored thanks to solution by 'Unnamed' on CodeWars

--- Beta/html_complementary_color.py
+++ Beta/html_complementary_color.py
@@ -1,13 +1,7 @@
-PAD_6 = '{:0>6}'.format
-TO_HEX = '{:02X}'.format
-
-
 def get_reversed_color(hex_color):
     if not isinstance(hex_color, str) or len(hex_color) >= 7:
         raise ValueError
-    padded = PAD_6(hex_color)
-    return '#{}'.format(''.join(TO_HEX(255 - int(padded[a:a + 2], 16))
-                                for a in xrange(0, 6, 2)))
+    return '#{:06X}'.format(0xffffff - int(hex_color or '0', 16))
 
 
 assert get_reversed_color('01fD08') == '#FE02F7'
