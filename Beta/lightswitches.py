18371a14cdc61ca7ec5964d296cc1b757d8bfee9
G <the-zebulan@users.noreply.github.com>
2016-02-16 12:01:57 -0700

    Adjusted range inside of xrange

--- Beta/lightswitches.py
+++ Beta/lightswitches.py
@@ -1,6 +1,6 @@
 def lightswitch(n):
     lights = [0] * n
-    for a in xrange(0, n + 1):
+    for a in xrange(n):
         for b in xrange(a, n, a + 1):
             lights[b] = int(not lights[b])
     return sum(lights)
