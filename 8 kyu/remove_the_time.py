665e849be6b0d560c32be90c5d610c12c0127634
G <the-zebulan@users.noreply.github.com>
2016-02-18 14:53:06 -0700

    Refactored thanks to a solution by GiacomoSorbi

--- 8 kyu/remove_the_time.py
+++ 8 kyu/remove_the_time.py
@@ -1,5 +1,5 @@
 def shorten_to_date(long_date):
-    return long_date[:long_date.rfind(',')]
+    return long_date.split(',')[0]
 
 
 assert shorten_to_date('Monday February 2, 8pm') == 'Monday February 2'
