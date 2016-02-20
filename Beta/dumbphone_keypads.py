0c37fad300a373b866e8d6a49646a1c6a9c072c7
G <the-zebulan@users.noreply.github.com>
2016-02-19 22:17:41 -0700

    Added 'dumbphone_keypads'

---
+++ Beta/dumbphone_keypads.py
@@ -0,0 +1,25 @@
+KEYPAD = {
+    ' ': '0', '0': '00', '1': '1', '2': '2222', '3': '3333', '4': '4444',
+    '5': '5555', '7': '77777', '8': '8888', '9': '99999', 'A': '2', 'B': '22',
+    'C': '222', 'D': '3', 'E': '33', 'F': '333', 'G': '4', 'H': '44',
+    'I': '444', 'J': '5', 'K': '55', 'L': '555', 'M': '6', 'N': '66',
+    'O': '666', 'P': '7', 'Q': '77', 'R': '777', 'S': '7777', 'T': '8',
+    'U': '88', 'V': '888', 'W': '9', 'X': '99', 'Y': '999', 'Z': '9999'
+}
+OUTPUT = '{}{}'.format
+
+
+def sequence(phrase):
+    prev = None
+    result = []
+    for a in phrase.upper():
+        current = KEYPAD.get(a, a)
+        first = current[0]
+        result.append(OUTPUT('p' if first == prev else '', current))
+        prev = first
+    return ''.join(result)
+
+
+assert sequence('HELLO WORLD') == '4433555p555666096667775553'
+assert sequence('codewar rocks') == '2226663p33927770777666222557777'
+assert sequence('#hashtag') == '#442777744824'
