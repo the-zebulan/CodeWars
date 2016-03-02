7fed483ef877268de78e9b8f307dc7c2482cf255
G <the-zebulan@users.noreply.github.com>
2016-03-01 21:05:05 -0700

    Removed assert statements

--- Beta/addition_function_problem.py
+++ Beta/addition_function_problem.py
@@ -1,4 +1,1 @@
 from operator import add
-
-assert add(1, 3) == 4
-assert add(2, 4) == 6
--- Beta/plus_1_array.py
+++ Beta/plus_1_array.py
@@ -1,9 +1,3 @@
 def up_array(arr):
     if arr and all(0 <= a < 10 and isinstance(a, int) for a in arr):
         return [int(c) for c in str(int(''.join(str(b) for b in arr)) + 1)]
-
-
-assert up_array([2, 3, 9]) == [2, 4, 0]
-assert up_array([4, 3, 2, 5]) == [4, 3, 2, 6]
-assert up_array([1, -9]) is None
-assert up_array([]) is None
