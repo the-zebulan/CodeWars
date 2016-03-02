f2b169a32db4401e6422038bacbfa99034fcbec7
G <the-zebulan@users.noreply.github.com>
2016-03-01 22:30:42 -0700

    Removed assert statements

--- Beta/array_2_binary.py
+++ Beta/array_2_binary.py
@@ -5,9 +5,3 @@ def arr2bin(arr):
             return False
         total += a
     return '{:b}'.format(total)
-
-assert arr2bin([1, 2]) == '11'
-assert arr2bin([1, 2, 'a']) is False
-assert arr2bin([1, 2, 3, 4, 5]) == "1111"
-assert arr2bin([1, 10, 100, 1000]) == "10001010111"
-assert arr2bin([94, 23, 73, True, 61]) is False
