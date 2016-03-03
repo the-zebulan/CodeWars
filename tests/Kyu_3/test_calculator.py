0a6e80a89e799e541e11deb51d0e664d48d5c702
G <the-zebulan@users.noreply.github.com>
2016-03-03 12:12:32 -0700

    Added Kyu_3 unittest

---
+++ tests/Kyu_3/test_calculator.py
@@ -0,0 +1,15 @@
+import unittest
+
+from Kyu_3.calculator import Calculator
+
+
+class CalculatorTestCase(unittest.TestCase):
+    def test_equals(self):
+        self.assertEqual(Calculator().evaluate('2 / 2 + 3 * 4 - 6'), 7)
+
+    def test_equals_2(self):
+        self.assertEqual(Calculator().evaluate('10 * 20 / 30'), 6)
+
+
+if __name__ == '__main__':
+    unittest.main()
