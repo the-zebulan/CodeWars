e32e216ae258f4168627584c11b0d438a5ea7f48
G <the-zebulan@users.noreply.github.com>
2016-03-07 17:34:55 -0700

    Added 'alien_beer_morse_code' and unittests

---
+++ tests/Beta/test_alien_beer_morse_code.py
@@ -0,0 +1,35 @@
+import unittest
+
+from Beta.alien_beer_morse_code import morse_converter
+
+
+class MorseConverterTestCase(unittest.TestCase):
+    def test_equals(self):
+        self.assertEqual(morse_converter(".----"), 1)
+
+    def test_equals_2(self):
+        self.assertEqual(morse_converter("..----------..."), 207)
+
+    def test_equals_3(self):
+        self.assertEqual(morse_converter("----.--.....---...--"), 9723)
+
+    def test_equals_4(self):
+        self.assertEqual(morse_converter('.----.----.----.----.----'), 11111)
+
+    def test_equals_5(self):
+        self.assertEqual(morse_converter(
+            '..----------...-....----------'), 207600)
+
+    def test_equals_6(self):
+        self.assertEqual(morse_converter('---------------'), 0)
+
+    def test_equals_7(self):
+        self.assertEqual(morse_converter('..---.--------....--'), 2193)
+
+    def test_equals_8(self):
+        self.assertEqual(morse_converter(
+            '.----..---...--....-.....-....--...---..----.-----'), 1234567890)
+
+
+if __name__ == '__main__':
+    unittest.main()
