ca1cc0347a16ec28309a50b480cbd52e76da592d
G <the-zebulan@users.noreply.github.com>
2016-03-12 14:23:59 -0700

    Modified import, pytest more flexible about function name

--- tests/Beta/test_thinking_and_testing_03.py
+++ tests/Beta/test_thinking_and_testing_03.py
@@ -1,47 +1,47 @@
 import unittest
 
-import Beta.thinking_and_testing_03
+from Beta.thinking_and_testing_03 import testit as solution
 
 
 class ThinkingAndTestingTestCase(unittest.TestCase):
     def test_equals(self):
-        self.assertEqual(Beta.thinking_and_testing_03.testit(0), 0)
+        self.assertEqual(solution(0), 0)
 
     def test_equals_2(self):
-        self.assertEqual(Beta.thinking_and_testing_03.testit(2), 1)
+        self.assertEqual(solution(2), 1)
 
     def test_equals_3(self):
-        self.assertEqual(Beta.thinking_and_testing_03.testit(3), 2)
+        self.assertEqual(solution(3), 2)
 
     def test_equals_4(self):
-        self.assertEqual(Beta.thinking_and_testing_03.testit(4), 1)
+        self.assertEqual(solution(4), 1)
 
     def test_equals_5(self):
-        self.assertEqual(Beta.thinking_and_testing_03.testit(5), 2)
+        self.assertEqual(solution(5), 2)
 
     def test_equals_6(self):
-        self.assertEqual(Beta.thinking_and_testing_03.testit(6), 2)
+        self.assertEqual(solution(6), 2)
 
     def test_equals_7(self):
-        self.assertEqual(Beta.thinking_and_testing_03.testit(7), 3)
+        self.assertEqual(solution(7), 3)
 
     def test_equals_8(self):
-        self.assertEqual(Beta.thinking_and_testing_03.testit(8), 1)
+        self.assertEqual(solution(8), 1)
 
     def test_equals_9(self):
-        self.assertEqual(Beta.thinking_and_testing_03.testit(9), 2)
+        self.assertEqual(solution(9), 2)
 
     def test_equals_10(self):
-        self.assertEqual(Beta.thinking_and_testing_03.testit(10), 2)
+        self.assertEqual(solution(10), 2)
 
     def test_equals_11(self):
-        self.assertEqual(Beta.thinking_and_testing_03.testit(100), 3)
+        self.assertEqual(solution(100), 3)
 
     def test_equals_12(self):
-        self.assertEqual(Beta.thinking_and_testing_03.testit(1000), 6)
+        self.assertEqual(solution(1000), 6)
 
     def test_equals_13(self):
-        self.assertEqual(Beta.thinking_and_testing_03.testit(10000), 5)
+        self.assertEqual(solution(10000), 5)
 
 
 if __name__ == '__main__':
