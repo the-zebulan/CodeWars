0664df84fc45d3cb941f675457f7f1ce1b7921aa
G <the-zebulan@users.noreply.github.com>
2016-03-12 15:03:08 -0700

    Added '__init__' and unittests for 'palindromes'

---
+++ tests/Retired/test_palindromes.py
@@ -0,0 +1,21 @@
+import unittest
+
+from Retired.palindromes import is_palindrome
+
+
+class IsPalindromeTestCase(unittest.TestCase):
+    def test_true(self):
+        self.assertTrue(is_palindrome(''))
+
+    def test_true_2(self):
+        self.assertTrue(is_palindrome('maoam'))
+
+    def test_true_3(self):
+        self.assertTrue(is_palindrome('If I had a hi-fi...'))
+
+    def test_false(self):
+        self.assertFalse(is_palindrome('abc'))
+
+
+if __name__ == '__main__':
+    unittest.main()
