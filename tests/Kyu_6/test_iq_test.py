f12902c27e8d9279ced723178abe9340d4946998
G <the-zebulan@users.noreply.github.com>
2016-03-12 14:53:29 -0700

    Modified import, pytest more flexible about function name

--- tests/Kyu_6/test_iq_test.py
+++ tests/Kyu_6/test_iq_test.py
@@ -1,18 +1,17 @@
 import unittest
 
-# from Kyu_6.iq_test import iq_test
-import Kyu_6.iq_test
+from Kyu_6.iq_test import iq_test
 
 
 class IQTestCase(unittest.TestCase):
     def test_equals(self):
-        self.assertEqual(Kyu_6.iq_test.iq_test('2 4 7 8 10'), 3)
+        self.assertEqual(iq_test('2 4 7 8 10'), 3)
 
     def test_equals_2(self):
-        self.assertEqual(Kyu_6.iq_test.iq_test('1 2 1 1'), 2)
+        self.assertEqual(iq_test('1 2 1 1'), 2)
 
     def test_equals_3(self):
-        self.assertEqual(Kyu_6.iq_test.iq_test('1 2 2'), 1)
+        self.assertEqual(iq_test('1 2 2'), 1)
 
 
 if __name__ == '__main__':
