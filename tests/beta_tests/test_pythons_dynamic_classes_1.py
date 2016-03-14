e784423e69de9f8579aa4633f4a089f87dab399c
G <the-zebulan@users.noreply.github.com>
2016-03-13 22:04:58 -0600

    Appended '_tests' to each sub-directory of 'tests'

--- tests/beta_tests/test_pythons_dynamic_classes_1.py
+++ tests/beta_tests/test_pythons_dynamic_classes_1.py
@@ -5,17 +5,17 @@ from Beta.pythons_dynamic_classes_1 import class_name_changer
 
 class ClassNameChangerTestCase(unittest.TestCase):
     def test_equals(self):
-        self.assertEqual(str(MyClass), '<class \'tests.beta.test_pythons_dyna'
+        self.assertEqual(str(MyClass), '<class \'tests.beta_tests.test_pythons_dyna'
                                        'mic_classes_1.MyClass\'>')
 
     def test_equals_2(self):
         class_name_changer(MyClass, 'UsefulClass')
-        self.assertEqual(str(MyClass), '<class \'tests.beta.test_pythons_dyna'
+        self.assertEqual(str(MyClass), '<class \'tests.beta_tests.test_pythons_dyna'
                                        'mic_classes_1.UsefulClass\'>')
 
     def test_equals_3(self):
         class_name_changer(MyClass, 'SecondUsefulClass')
-        self.assertEqual(str(MyClass), '<class \'tests.beta.test_pythons_dyna'
+        self.assertEqual(str(MyClass), '<class \'tests.beta_tests.test_pythons_dyna'
                                        'mic_classes_1.SecondUsefulClass\'>')
 
 
