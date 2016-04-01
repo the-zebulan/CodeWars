026b3c129a48d110a0c4eed07ef9c79c1b26ab73
G <the-zebulan@users.noreply.github.com>
2016-04-01 15:41:24 -0600

    Added 'do_i_get_a_bonus' and unittests

---
+++ katas/kyu_8/do_i_get_a_bonus.py
@@ -0,0 +1,2 @@
+def bonus_time(salary, bonus):
+    return '${}'.format(salary * (10 if bonus else 1))
---
+++ tests/kyu_8_tests/test_do_i_get_a_bonus.py
@@ -0,0 +1,30 @@
+import unittest
+
+from katas.kyu_8.do_i_get_a_bonus import bonus_time
+
+
+class BonusTimeTestCase(unittest.TestCase):
+    def test_equals(self):
+        self.assertEqual(bonus_time(10000, True), '$100000')
+
+    def test_equals_2(self):
+        self.assertEqual(bonus_time(25000, True), '$250000')
+
+    def test_equals_3(self):
+        self.assertEqual(bonus_time(10000, False), '$10000')
+
+    def test_equals_4(self):
+        self.assertEqual(bonus_time(60000, False), '$60000')
+
+    def test_equals_5(self):
+        self.assertEqual(bonus_time(2, True), '$20')
+
+    def test_equals_6(self):
+        self.assertEqual(bonus_time(78, False), '$78')
+
+    def test_equals_7(self):
+        self.assertEqual(bonus_time(67890, True), '$678900')
+
+
+if __name__ == '__main__':
+    unittest.main()
