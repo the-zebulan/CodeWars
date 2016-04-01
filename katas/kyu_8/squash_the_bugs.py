59d5628cfc4754b5047dc8a5f71fb24b385268ac
G <the-zebulan@users.noreply.github.com>
2016-04-01 15:35:26 -0600

    Added 'squash_the_bugs' and unittests

---
+++ katas/kyu_8/squash_the_bugs.py
@@ -0,0 +1,2 @@
+def find_longest(strng):
+    return max(len(a) for a in strng.split())
---
+++ tests/kyu_8_tests/test_squash_the_bugs.py
@@ -0,0 +1,29 @@
+import unittest
+
+from katas.kyu_8.squash_the_bugs import find_longest
+
+
+class SquashTheBugsTestCase(unittest.TestCase):
+    def test_equals(self):
+        self.assertEqual(find_longest(
+            'The quick white fox jumped around the massive dog'
+            ), 7
+        )
+
+    def test_equals_2(self):
+        self.assertEqual(find_longest('Take me to tinseltown with you'), 10)
+
+    def test_equals_3(self):
+        self.assertEqual(find_longest('Sausage chops'), 7)
+
+    def test_equals_4(self):
+        self.assertEqual(
+            find_longest('Wind your body and wiggle your belly'), 6
+        )
+
+    def test_equals_5(self):
+        self.assertEqual(find_longest('Lets all go on holiday'), 7)
+
+
+if __name__ == '__main__':
+    unittest.main()
