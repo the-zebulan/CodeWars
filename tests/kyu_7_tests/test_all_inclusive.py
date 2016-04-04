107b30f335e2ed4c316b8be21b0995dd6ec93d8d
G <the-zebulan@users.noreply.github.com>
2016-04-04 14:52:23 -0600

    Fixed PEP8 - lines too long

--- tests/kyu_7_tests/test_all_inclusive.py
+++ tests/kyu_7_tests/test_all_inclusive.py
@@ -5,7 +5,9 @@ from katas.kyu_7.all_inclusive import contain_all_rots
 
 class RotationsTestCase(unittest.TestCase):
     def test_true(self):
-        self.assertTrue(contain_all_rots('bsjq', ['bsjq', 'qbsj', 'sjqb', 'twZNsslC', 'jqbs']))
+        self.assertTrue(contain_all_rots(
+            'bsjq', ['bsjq', 'qbsj', 'sjqb', 'twZNsslC', 'jqbs'])
+        )
 
     def test_true_2(self):
         self.assertTrue(contain_all_rots('', []))
@@ -14,10 +16,16 @@ class RotationsTestCase(unittest.TestCase):
         self.assertTrue(contain_all_rots('', ['bsjq', 'qbsj']))
 
     def test_false(self):
-        self.assertFalse(contain_all_rots('Ajylvpy', ['Ajylvpy', 'ylvpyAj', 'jylvpyA', 'lvpyAjy', 'pyAjylv', 'vpyAjyl', 'ipywee']))
+        self.assertFalse(contain_all_rots('Ajylvpy', [
+            'Ajylvpy', 'ylvpyAj', 'jylvpyA', 'lvpyAjy', 'pyAjylv',
+            'vpyAjyl', 'ipywee'
+        ]))
 
     def test_false_2(self):
-        self.assertFalse(contain_all_rots('XjYABhR', ['TzYxlgfnhf', 'yqVAuoLjMLy', 'BhRXjYA', 'YABhRXj', 'hRXjYAB', 'jYABhRX', 'XjYABhR', 'ABhRXjY']))
+        self.assertFalse(contain_all_rots('XjYABhR', [
+            'TzYxlgfnhf', 'yqVAuoLjMLy', 'BhRXjYA', 'YABhRXj', 'hRXjYAB',
+            'jYABhRX', 'XjYABhR', 'ABhRXjY'
+        ]))
 
 
 if __name__ == '__main__':
