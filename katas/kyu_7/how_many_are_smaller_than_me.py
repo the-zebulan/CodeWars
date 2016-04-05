df2dd47e5fe1a4ecc30e11ac4ba72ac6a0b14648
G <the-zebulan@users.noreply.github.com>
2016-04-05 14:52:32 -0600

    Refactored old solution

--- katas/kyu_7/how_many_are_smaller_than_me.py
+++ katas/kyu_7/how_many_are_smaller_than_me.py
@@ -1,9 +1,2 @@
 def smaller(arr):
-    right_smaller = {}
-    for dex_val in enumerate(arr):
-        right_smaller.setdefault(dex_val, 0)
-        val = dex_val[1]
-        for key in right_smaller:
-            if dex_val != key and val < key[1]:
-                right_smaller[key] += 1
-    return [right_smaller[b] for b in enumerate(arr)]
+    return [sum(b < a for b in arr[i + 1:]) for i, a in enumerate(arr)]
