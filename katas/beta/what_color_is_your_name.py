6cdb3837f89495d5badefe01201a596160e32da7
G <the-zebulan@users.noreply.github.com>
2016-04-08 22:49:59 -0600

    Refactored, function name -> PEP8, length < 2 must return None

--- katas/beta/what_color_is_your_name.py
+++ katas/beta/what_color_is_your_name.py
@@ -1,14 +1,17 @@
-def stringColor(name):
-    first_char = other_chars = sum_chars = 0
+def string_color(name):
+    first_char = length = other_chars = sum_chars = 0
     prod_chars = 1
     for i, a in enumerate(name):
         current = ord(a)
+        length += 1
         if i == 0:
             first_char = current
         else:
             other_chars += current
         prod_chars *= current
         sum_chars += current
+    if length < 2:
+        return None
     return '{:02X}{:02X}{:02X}'.format(
         sum_chars % 255, prod_chars % 255, abs(first_char - other_chars) % 255
     )
