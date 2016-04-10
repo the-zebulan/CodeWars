4348330ddb35d7cdc0c9cce8a0cbe94fe0763709
G <the-zebulan@users.noreply.github.com>
2016-04-09 18:25:33 -0600

    Added 'multiply_the_number' and unittests

---
+++ katas/kyu_8/multiply_the_number.py
@@ -0,0 +1,2 @@
+def multiply(n):
+    return n * (5 ** len(str(abs(n))))
---
+++ tests/kyu_8_tests/test_multiply_the_number.py
@@ -0,0 +1,24 @@
+import unittest
+
+from katas.kyu_8.multiply_the_number import multiply
+
+
+class MyTestCase(unittest.TestCase):
+    def test_equals(self):
+        self.assertEqual(multiply(10), 250)
+
+    def test_equals_2(self):
+        self.assertEqual(multiply(5), 25)
+
+    def test_equals_3(self):
+        self.assertEqual(multiply(200), 25000)
+
+    def test_equals_4(self):
+        self.assertEqual(multiply(0), 0)
+
+    def test_equals_5(self):
+        self.assertEqual(multiply(-2), -10)
+
+
+if __name__ == '__main__':
+    unittest.main()
