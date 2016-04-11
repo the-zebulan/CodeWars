351773c262836954a6e64714bfc331e0d7acdea0
G <the-zebulan@users.noreply.github.com>
2016-04-10 22:46:32 -0600

    Added 'wilson_primes' and unittests

---
+++ katas/kyu_8/wilson_primes.py
@@ -0,0 +1,2 @@
+def am_i_wilson(n):
+    return n in (5, 13, 563)
---
+++ tests/beta_tests/test_wilson_primes.py
@@ -0,0 +1,30 @@
+import unittest
+
+from katas.kyu_8.wilson_primes import am_i_wilson
+
+
+class WilsonPrimesTestCase(unittest.TestCase):
+    def test_true(self):
+        self.assertTrue(am_i_wilson(5))
+
+    def test_true_2(self):
+        self.assertTrue(am_i_wilson(13))
+
+    def test_true_3(self):
+        self.assertTrue(am_i_wilson(563))
+
+    def test_false(self):
+        self.assertFalse(am_i_wilson(0))
+
+    def test_false_2(self):
+        self.assertFalse(am_i_wilson(1))
+
+    def test_false_3(self):
+        self.assertFalse(am_i_wilson(8))
+
+    def test_false_4(self):
+        self.assertFalse(am_i_wilson(9))
+
+
+if __name__ == '__main__':
+    unittest.main()
