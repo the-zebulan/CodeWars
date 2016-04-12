1e7ff5e13401e34867afcd9827c645742a0a330c
G <the-zebulan@users.noreply.github.com>
2016-04-12 16:30:38 -0600

    Minor refactor, adjusted regex to use \Z, got rid of a check

--- katas/kyu_6/regexp_basics_parsing_time.py
+++ katas/kyu_6/regexp_basics_parsing_time.py
@@ -1,10 +1,10 @@
 from re import compile, match
 
-REGEX = compile(r'^(?P<h>\d{2}):(?P<m>[0-5]\d):(?P<s>[0-5]\d)$')
+REGEX = compile(r'^(?P<h>\d{2}):(?P<m>[0-5]\d):(?P<s>[0-5]\d)\Z')
 
 
 def to_seconds(time):
     m = match(REGEX, time)
-    if m and time[slice(*m.span())] == time:
+    if m:
         hms = m.groupdict()
         return int(hms['h']) * 3600 + int(hms['m']) * 60 + int(hms['s'])
