5e0368a5d910cac27203a670de77cf75b3083ec0
G <the-zebulan@users.noreply.github.com>
2016-04-13 10:11:17 -0600

    Added 'number_to_bytes' and unittests

---
+++ katas/beta/number_to_bytes.py
@@ -0,0 +1,8 @@
+from math import ceil
+
+
+def toBytes(n):
+    binary = '{:b}'.format(n)
+    width = int(ceil(len(binary) / 8.0) * 8)
+    padded = binary.zfill(width)
+    return [padded[a:a + 8] for a in xrange(0, width, 8)]
---
+++ tests/beta_tests/test_number_to_bytes.py
@@ -0,0 +1,31 @@
+import unittest
+
+from katas.beta.number_to_bytes import toBytes
+
+
+class NumberToBytesTestCase(unittest.TestCase):
+    def test_equals(self):
+        self.assertEqual(toBytes(0), ['00000000'])
+
+    def test_equals_2(self):
+        self.assertEqual(toBytes(1), ['00000001'])
+
+    def test_equals_3(self):
+        self.assertEqual(toBytes(257), ['00000001', '00000001'])
+
+    def test_equals_4(self):
+        self.assertEqual(toBytes(0x101), ['00000001', '00000001'])
+
+    def test_equals_5(self):
+        self.assertEqual(toBytes(0x000000000101), ['00000001', '00000001'])
+
+    def test_equals_6(self):
+        self.assertEqual(toBytes(0xffff), ['11111111', '11111111'])
+
+    def test_equals_7(self):
+        self.assertEqual(toBytes(0x1020304),
+                         ['00000001', '00000010', '00000011', '00000100'])
+
+
+if __name__ == '__main__':
+    unittest.main()
