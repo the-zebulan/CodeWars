629900e27e5787194ef14cb4cf6ac7df6e0f7bd3
the-zebulan <the-zebulan@users.noreply.github.com>
2016-05-05 01:30:37 -0600

    Refactored using collections.deque, 20-25% speedup

--- katas/beta/count_inversions.py
+++ katas/beta/count_inversions.py
@@ -1,2 +1,16 @@
-def count_inversion(nums):
-    return sum(a > b for i, a in enumerate(nums) for b in nums[i + 1:])
+from collections import deque
+
+
+# def count_inversion(nums):  # approx 8.3 seconds per 100,000
+#     return sum(a > b for i, a in enumerate(nums) for b in nums[i + 1:])
+
+
+def count_inversion(nums):  # approx 6.6 seconds per 100,000
+    forward_nums = deque(nums)
+    result = 0
+    for a in nums:
+        forward_nums.popleft()
+        for b in forward_nums:
+            if a > b:
+                result += 1
+    return result
