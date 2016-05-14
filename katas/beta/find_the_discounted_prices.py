819865441fee3b21ee848ee45b3db2ff18381393
the-zebulan <the-zebulan@users.noreply.github.com>
2016-05-13 22:01:05 -0600

    Added 'find_the_discounted_prices' and unittests

---
+++ katas/beta/find_the_discounted_prices.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+
+def find_discounted(prices):
+    count_prices = Counter(int(a) for a in prices.split())
+    result = []
+    for key in sorted(count_prices):
+        value = count_prices[key]
+        try:
+            count_prices[int(key / 0.75)] -= value
+        except KeyError:
+            continue
+        result.extend(key for _ in xrange(value if key else 1))
+    return ' '.join(str(b) for b in result)
