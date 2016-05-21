6c8db323d24eba2870b438700b2e6fea985a4321
the-zebulan <the-zebulan@users.noreply.github.com>
2016-05-21 00:39:14 -0600

    Minor refactor

--- katas/beta/bubble_sort.py
+++ katas/beta/bubble_sort.py
@@ -1,14 +1,12 @@
 def bubble(lst):
     length = len(lst) - 1
     result = []
-    no_swap = True
-    while True:
-        for a in xrange(length):
+    no_swap = False
+    while not no_swap:
+        no_swap = True
+        for a in range(length):
             if lst[a] > lst[a + 1]:
                 lst[a], lst[a + 1] = lst[a + 1], lst[a]
                 result.append(list(lst))
                 no_swap = False
-        if no_swap:
-            break
-        no_swap = True
     return result
