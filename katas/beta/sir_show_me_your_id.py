1fc6301c65577919a258662e6d5be3510b22f82f
the-zebulan <the-zebulan@users.noreply.github.com>
2016-06-12 12:06:01 -0600

    Katas test cases were updated on Codewars, refactored regex & tests

--- katas/beta/sir_show_me_your_id.py
+++ katas/beta/sir_show_me_your_id.py
@@ -2,4 +2,4 @@ import re
 
 
 def show_me(name):
-    return bool(re.match(r'(-[A-Z][a-zA-Z]+)+$', '-' + name))
+    return bool(re.match(r'(-[A-Z][a-z]+)+$', '-' + name))
--- tests/beta_tests/test_sir_show_me_your_id.py
+++ tests/beta_tests/test_sir_show_me_your_id.py
@@ -5,19 +5,40 @@ from katas.beta.sir_show_me_your_id import show_me
 
 class ShowMeTestCase(unittest.TestCase):
     def test_true_1(self):
-        self.assertTrue(show_me("Francis"))
+        self.assertTrue(show_me('Francis'))
 
     def test_true_2(self):
-        self.assertTrue(show_me("Jean-Eluard"))
+        self.assertTrue(show_me('Jean-Eluard'))
 
     def test_true_3(self):
-        self.assertTrue(show_me("Bernard-Henry-Levy"))
+        self.assertTrue(show_me('Bernard-Henry-Levy'))
 
     def test_false_1(self):
-        self.assertFalse(show_me("Le Mec"))
+        self.assertFalse(show_me('Le Mec'))
 
     def test_false_2(self):
-        self.assertFalse(show_me("Meme Gertrude"))
+        self.assertFalse(show_me('Meme Gertrude'))
+
+    def test_false_3(self):
+        self.assertFalse(show_me('A-a-a-a----a-a'))
+
+    def test_false_4(self):
+        self.assertFalse(show_me('Z-------------'))
+
+    def test_false_5(self):
+        self.assertFalse(show_me('Jean-luc'))
+
+    def test_false_6(self):
+        self.assertFalse(show_me('Jean--Luc'))
+
+    def test_false_7(self):
+        self.assertFalse(show_me('JeanLucPicard'))
+
+    def test_false_8(self):
+        self.assertFalse(show_me('-Jean-Luc'))
+
+    def test_false_9(self):
+        self.assertFalse(show_me('Jean-Luc-Picard-'))
 
 
 if __name__ == '__main__':
