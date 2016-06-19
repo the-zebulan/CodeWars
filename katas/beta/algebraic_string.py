35cfb5016e0e4456f0ed1dff0389f21fbf64ac8d
the-zebulan <the-zebulan@users.noreply.github.com>
2016-06-19 00:30:48 -0600

    Katas function name updated to PEP8 style `snake_case`

--- katas/beta/algebraic_string.py
+++ katas/beta/algebraic_string.py
@@ -3,5 +3,5 @@
 # sys.setrecursionlimit(1700)  # currently needed for Python 3 version
 
 
-def SumProd(str_expression):
+def sum_prod(str_expression):
     return '{:.5e}'.format(eval(str_expression))
