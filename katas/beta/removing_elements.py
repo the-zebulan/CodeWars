446fda0f3d581fc86872a0e7dcb81774c2c2a885
the-zebulan <the-zebulan@users.noreply.github.com>
2016-06-23 22:49:09 -0600

    Added 'removing_elements' and unittests

---
+++ katas/beta/removing_elements.py
@@ -0,0 +1,2 @@
+def remove_every_other(my_list):
+    return my_list[::2]
---
+++ tests/beta_tests/test_removing_elements.py
@@ -0,0 +1,33 @@
+import unittest
+
+from katas.beta.removing_elements import remove_every_other
+
+
+class RemoveEveryOtherTestCase(unittest.TestCase):
+    def test_equal_1(self):
+        self.assertEqual(remove_every_other([1]), [1])
+
+    def test_equal_2(self):
+        self.assertEqual(remove_every_other([[1, 2, 3, 4, 5]]),
+                         [[1, 2, 3, 4, 5]])
+
+    def test_equal_3(self):
+        self.assertEqual(remove_every_other(['Hello', 'Goodbye']), ['Hello'])
+
+    def test_equal_4(self):
+        self.assertEqual(remove_every_other([1.013, 2398.00]), [1.013])
+
+    def test_equal_5(self):
+        self.assertEqual(
+            remove_every_other(['Yes', 'No', 'Yes', 'No', 'Yes', 'No']),
+            ['Yes', 'Yes', 'Yes']
+        )
+
+    def test_equal_6(self):
+        self.assertEqual(remove_every_other(
+            [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6, 7], [8, 9, 10, 11, 12]]
+        ), [[1, 2], [3, 4], [5, 6, 7]])
+
+
+if __name__ == '__main__':
+    unittest.main()
