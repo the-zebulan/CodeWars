aab261fcfca62ff9e0c0d2f8c06c44b302b646f2
the-zebulan <the-zebulan@users.noreply.github.com>
2016-07-11 21:49:42 -0600

    Added 'find_jon_snow_parents' and unittests

---
+++ katas/beta/find_jon_snow_parents.py
@@ -0,0 +1,4 @@
+def jonSnowParents(dad, mom):
+    if dad == 'Rhaegar Targaryen' and mom == 'Lyanna Stark':
+        return 'Jon Snow you deserve the throne'
+    return 'Jon Snow, you know nothing'
---
+++ tests/beta_tests/test_find_jon_snow_parents.py
@@ -0,0 +1,30 @@
+import unittest
+
+from katas.beta.find_jon_snow_parents import jonSnowParents
+
+
+class JonSnowParentsTestCase(unittest.TestCase):
+    def test_equal_1(self):
+        self.assertEqual(jonSnowParents('Robert Baratheon', 'Catelyn Stark'),
+                         'Jon Snow, you know nothing')
+
+    def test_equal_2(self):
+        self.assertEqual(jonSnowParents('Lyanna Stark', 'Rhaegar Targaryen'),
+                         'Jon Snow, you know nothing')
+
+    def test_equal_3(self):
+        self.assertEqual(jonSnowParents('Barack Obama', 'Lady Gaga'),
+                         'Jon Snow, you know nothing')
+
+    def test_equal_4(self):
+        self.assertEqual(jonSnowParents('Rhaegar Targaryen', 'Lyanna Stark'),
+                         'Jon Snow you deserve the throne')
+
+    def test_not_equal_1(self):
+        self.assertNotEqual(
+            jonSnowParents('Rhaegar Targaryen', 'Lyanna Stark'),
+            'Jon Snow, you know nothing'
+        )
+
+if __name__ == '__main__':
+    unittest.main()
