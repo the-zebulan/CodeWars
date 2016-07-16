40098fe1cb73cee2fb5be86421cb674a9c0e69f5
the-zebulan <the-zebulan@users.noreply.github.com>
2016-07-15 23:48:49 -0600

    Forgot to add `random.seed` back in

--- tests/kyu_6_tests/test_dont_rely_on_luck.py
+++ tests/kyu_6_tests/test_dont_rely_on_luck.py
@@ -1,12 +1,14 @@
 import unittest
-from random import randint
+from random import randint, seed
 
 from katas.kyu_6.dont_rely_on_luck import guess
 
 
 class DontRelyOnLuckTestCase(unittest.TestCase):
     def test_equal_1(self):
+        seed(1)
         self.assertEqual(guess, randint(1, 100))
 
     def test_not_equal_1(self):
+        seed(22)
         self.assertNotEqual(guess, randint(1, 100))
