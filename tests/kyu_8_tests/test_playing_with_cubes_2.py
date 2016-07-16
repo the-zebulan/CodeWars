9a00da4d5e3e9bbb36257d3d45c3cd5e47a9bcd3
the-zebulan <the-zebulan@users.noreply.github.com>
2016-07-15 23:26:19 -0600

    Added a test for code coverage

--- tests/kyu_8_tests/test_playing_with_cubes_2.py
+++ tests/kyu_8_tests/test_playing_with_cubes_2.py
@@ -4,8 +4,13 @@ from katas.kyu_8.playing_with_cubes_2 import Cube
 
 
 class CubeTestCase(unittest.TestCase):
-    def test_equals(self):
+    def test_equal_1(self):
         self.assertEqual(Cube(10).get_side(), 10)
 
-    def test_equals_2(self):
+    def test_equal_2(self):
         self.assertEqual(Cube().get_side(), 0)
+
+    def test_equal_3(self):
+        c = Cube(2)
+        c.set_side(3)
+        self.assertEqual(c.get_side(), 3)
