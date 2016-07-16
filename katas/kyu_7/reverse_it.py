6a0b4e6b8c9ed03b305b006c49ef6f0e96900604
the-zebulan <the-zebulan@users.noreply.github.com>
2016-07-15 23:04:44 -0600

    Added tests for code coverage, minor refactor of solution code

--- katas/kyu_7/reverse_it.py
+++ katas/kyu_7/reverse_it.py
@@ -1,15 +1,4 @@
-# def reverse_it(data):
-#     if isinstance(data, bool) or not isinstance(data, (int, float, str)):
-#         return data
-#     elif isinstance(data, str):
-#         return data[::-1]
-#     elif isinstance(data, int):
-#         return int(str(data)[::-1])
-#     else:  # float
-#         return float(str(data)[::-1])
-
-
 def reverse_it(data):
-    if type(data) in (int, str, float):
+    if isinstance(data, (int, float, str)):
         return type(data)(str(data)[::-1])
     return data
--- tests/kyu_7_tests/test_reverse_it.py
+++ tests/kyu_7_tests/test_reverse_it.py
@@ -15,3 +15,10 @@ class ReverseItTestCase(unittest.TestCase):
 
     def test_equals_4(self):
         self.assertEqual(reverse_it('123.123'), '321.321')
+
+    def test_equal_5(self):
+        func = lambda: 1
+        self.assertEqual(reverse_it(func), func)
+
+    def test_equal_6(self):
+        self.assertEqual(reverse_it([1, 2, 3, 4]), [1, 2, 3, 4])
