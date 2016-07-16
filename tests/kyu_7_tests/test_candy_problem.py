2ff25b0bef21916bee7423dd1512c90365f462c5
the-zebulan <the-zebulan@users.noreply.github.com>
2016-07-15 23:14:47 -0600

    Added a test for code coverage

--- tests/kyu_7_tests/test_candy_problem.py
+++ tests/kyu_7_tests/test_candy_problem.py
@@ -4,14 +4,17 @@ from katas.kyu_7.candy_problem import candies
 
 
 class CandiesTestCase(unittest.TestCase):
-    def test_equals(self):
+    def test_equal_1(self):
         self.assertEqual(candies([5, 8, 6, 4]), 9)
 
-    def test_equals_2(self):
+    def test_equal_2(self):
         self.assertEqual(candies([1, 2, 4, 6]), 11)
 
-    def test_equals_3(self):
+    def test_equal_3(self):
         self.assertEqual(candies([1, 6]), 5)
 
-    def test_equals_4(self):
+    def test_equal_4(self):
         self.assertEqual(candies([]), -1)
+
+    def test_equal_5(self):
+        self.assertEqual(candies([1, 2, None, 3]), -1)
