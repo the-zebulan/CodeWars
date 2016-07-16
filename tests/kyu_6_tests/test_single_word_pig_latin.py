1344a5720aa1e4ca8a803bb78e5a317b0bcf5fc5
the-zebulan <the-zebulan@users.noreply.github.com>
2016-07-15 22:15:05 -0600

    Added tests for code coverage

--- tests/kyu_6_tests/test_single_word_pig_latin.py
+++ tests/kyu_6_tests/test_single_word_pig_latin.py
@@ -4,11 +4,20 @@ from katas.kyu_6.single_word_pig_latin import pig_latin
 
 
 class PigLatinTestCase(unittest.TestCase):
-    def test_equals(self):
+    def test_equal_1(self):
         self.assertEqual(pig_latin('map'), 'apmay')
 
-    def test_equals_2(self):
+    def test_equal_2(self):
         self.assertEqual(pig_latin('egg'), 'eggway')
 
-    def test_equals_3(self):
+    def test_equal_3(self):
         self.assertEqual(pig_latin('spaghetti'), 'aghettispay')
+
+    def test_equal_4(self):
+        self.assertEqual(pig_latin('zqxy'), 'zqxyay')
+
+    def test_is_none_1(self):
+        self.assertIsNone(pig_latin(''))
+
+    def test_is_none_2(self):
+        self.assertIsNone(pig_latin('!@#$%'))
