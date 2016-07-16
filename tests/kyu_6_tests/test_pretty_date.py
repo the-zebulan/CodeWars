1f770780fc5b5039f750323f12e55f3a30b7706d
the-zebulan <the-zebulan@users.noreply.github.com>
2016-07-15 22:08:32 -0600

    Added a test for code coverage

--- tests/kyu_6_tests/test_pretty_date.py
+++ tests/kyu_6_tests/test_pretty_date.py
@@ -4,11 +4,14 @@ from katas.kyu_6.pretty_date import to_pretty
 
 
 class PrettyDateTestCase(unittest.TestCase):
-    def test_equals(self):
+    def test_equal_1(self):
         self.assertEqual(to_pretty(300), '5 minutes ago')
 
-    def test_equals_2(self):
+    def test_equal_2(self):
         self.assertEqual(to_pretty(60), 'a minute ago')
 
-    def test_equals_3(self):
+    def test_equal_3(self):
         self.assertEqual(to_pretty(3600), 'an hour ago')
+
+    def test_equal_4(self):
+        self.assertEqual(to_pretty(0), 'just now')
