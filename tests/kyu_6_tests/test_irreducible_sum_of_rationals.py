4fac7f4cafc87f8173df7ec0ccedf2d8dac4065d
the-zebulan <the-zebulan@users.noreply.github.com>
2016-07-15 22:04:05 -0600

    Added a test for code coverage

--- tests/kyu_6_tests/test_irreducible_sum_of_rationals.py
+++ tests/kyu_6_tests/test_irreducible_sum_of_rationals.py
@@ -4,8 +4,11 @@ from katas.kyu_6.irreducible_sum_of_rationals import sum_fracts
 
 
 class SumOfRationalsTestCase(unittest.TestCase):
-    def test_equals(self):
+    def test_equal_1(self):
         self.assertEqual(sum_fracts([[1, 2], [1, 3], [1, 4]]), [13, 12])
 
-    def test_equals_2(self):
+    def test_equal_2(self):
         self.assertEqual(sum_fracts([[1, 3], [5, 3]]), 2)
+
+    def test_is_none_1(self):
+        self.assertIsNone(sum_fracts([[0, 2], [0, 2]]))
