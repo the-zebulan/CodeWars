9270894689c499c3029167a1313156b0ccf5e5ae
the-zebulan <the-zebulan@users.noreply.github.com>
2016-07-15 23:07:17 -0600

    Added a test for code coverage

--- tests/kyu_7_tests/test_maximum_length_difference.py
+++ tests/kyu_7_tests/test_maximum_length_difference.py
@@ -4,8 +4,11 @@ from katas.kyu_7.maximum_length_difference import mxdiflg
 
 
 class MaximumLengthDifferenceTestCase(unittest.TestCase):
-    def test_equals(self):
+    def test_equal_1(self):
         self.assertEqual(mxdiflg([
             'hoqq', 'bbllkw', 'oox', 'ejjuyyy', 'plmiis', 'xxxzgpsssa',
             'xxwwkktt', 'znnnnfqknaz', 'qqquuhii', 'dvvvwz'
         ], ['cccooommaaqqoxii', 'gggqaffhhh', 'tttoowwwmmww']), 13)
+
+    def test_equal_2(self):
+        self.assertEqual(mxdiflg(['abc'], []), -1)
