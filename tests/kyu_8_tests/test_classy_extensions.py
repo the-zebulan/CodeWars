2d5eac31cdf961a3087e3afdc65c25eee7329fe3
the-zebulan <the-zebulan@users.noreply.github.com>
2016-07-15 23:23:13 -0600

    Added a test for code coverage

--- tests/kyu_8_tests/test_classy_extensions.py
+++ tests/kyu_8_tests/test_classy_extensions.py
@@ -1,15 +1,18 @@
 import unittest
 
-from katas.kyu_8.classy_extensions import Cat
+from katas.kyu_8.classy_extensions import Animal, Cat
 
 
 class CatTestCase(unittest.TestCase):
-    def test_equals(self):
+    def test_equal_1(self):
         self.assertEqual(Cat('Mr Whiskers').speak(), 'Mr Whiskers meows.')
 
-    def test_equals_2(self):
+    def test_equal_2(self):
         self.assertEqual(Cat('Lamp').speak(), 'Lamp meows.')
 
-    def test_equals_3(self):
+    def test_equal_3(self):
         self.assertEqual(Cat('$$Money Bags$$').speak(),
                          '$$Money Bags$$ meows.')
+
+    def test_equal_4(self):
+        self.assertEqual(Animal('Rover').speak(), 'Some other message?!?!')
