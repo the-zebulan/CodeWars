fbc89cc887458688b3779def88939c7bdf6c28ba
the-zebulan <the-zebulan@users.noreply.github.com>
2016-07-15 22:06:10 -0600

    Added a test for code coverage

--- tests/kyu_6_tests/test_eighties_kids_7_shes_a_small_wonder.py
+++ tests/kyu_6_tests/test_eighties_kids_7_shes_a_small_wonder.py
@@ -7,11 +7,11 @@ class RobotTestCase(unittest.TestCase):
     def setUp(self):
         self.vicky = Robot()
 
-    def test_equals(self):
+    def test_equal_1(self):
         self.assertEqual(self.vicky.learn_word('hello'),
                          'Thank you for teaching me hello')
 
-    def test_equals_2(self):
+    def test_equal_2(self):
         self.assertEqual(self.vicky.learn_word('world'),
                          'Thank you for teaching me world')
         self.assertEqual(self.vicky.learn_word('World'),
@@ -19,10 +19,14 @@ class RobotTestCase(unittest.TestCase):
         self.assertEqual(self.vicky.learn_word('world'),
                          'I already know the word world')
 
-    def test_equals_3(self):
+    def test_equal_3(self):
         self.assertEqual(self.vicky.learn_word('goodbye'),
                          'Thank you for teaching me goodbye')
 
-    def test_equals_4(self):
+    def test_equal_4(self):
         self.assertEqual(self.vicky.learn_word('Thank'),
                          'I already know the word Thank')
+
+    def test_equal_5(self):
+        self.assertEqual(self.vicky.learn_word('!@#$%^'),
+                         'I do not understand the input')
