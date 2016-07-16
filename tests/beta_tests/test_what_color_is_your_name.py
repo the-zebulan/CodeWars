4e93f7b50d6a62acea0045c32d7cd26899da012b
the-zebulan <the-zebulan@users.noreply.github.com>
2016-07-16 00:03:46 -0600

    Added a test for code coverage

--- tests/beta_tests/test_what_color_is_your_name.py
+++ tests/beta_tests/test_what_color_is_your_name.py
@@ -4,17 +4,20 @@ from katas.beta.what_color_is_your_name import string_color
 
 
 class StringColorTestCase(unittest.TestCase):
-    def test_equals(self):
+    def test_equal_1(self):
         self.assertEqual(string_color('Jack'), '79CAE5')
 
-    def test_equals_2(self):
+    def test_equal_2(self):
         self.assertEqual(string_color('Joshua'), '6A10D6')
 
-    def test_equals_3(self):
+    def test_equal_3(self):
         self.assertEqual(string_color('Joshua Smith'), '8F00FB')
 
-    def test_equals_4(self):
+    def test_equal_4(self):
         self.assertEqual(string_color('Hayden Smith'), '7E00EE')
 
-    def test_equals_5(self):
+    def test_equal_5(self):
         self.assertEqual(string_color('Mathew Smith'), '8B00F1')
+
+    def test_is_none_1(self):
+        self.assertIsNone(string_color('a'))
