b4e5548a3c249c06af1198e0a000937dac0bf9c3
the-zebulan <the-zebulan@users.noreply.github.com>
2016-07-31 09:18:09 -0600

    Minor rename of tests

--- tests/kyu_8_tests/test_add_length.py
+++ tests/kyu_8_tests/test_add_length.py
@@ -4,15 +4,15 @@ from katas.kyu_8.add_length import add_length
 
 
 class AddLengthTestCase(unittest.TestCase):
-    def test_equals(self):
+    def test_equal_1(self):
         self.assertEqual(add_length('apple ban'), ['apple 5', 'ban 3'])
 
-    def test_equals_2(self):
+    def test_equal_2(self):
         self.assertEqual(add_length('you will win'),
                          ['you 3', 'will 4', 'win 3'])
 
-    def test_equals_3(self):
+    def test_equal_3(self):
         self.assertEqual(add_length('you'), ['you 3'])
 
-    def test_equals_4(self):
+    def test_equal_4(self):
         self.assertEqual(add_length('y'), ['y 1'])
