7d9ee472947a3d7e5d35180319226bb2afcf6132
the-zebulan <the-zebulan@users.noreply.github.com>
2016-08-02 12:47:13 -0600

    Double quotes to single quotes

--- tests/kyu_8_tests/test_are_they_opposite.py
+++ tests/kyu_8_tests/test_are_they_opposite.py
@@ -5,16 +5,16 @@ from katas.kyu_8.are_they_opposite import isOpposite
 
 class IsOppositeTestCase(unittest.TestCase):
     def test_true_1(self):
-        self.assertTrue(isOpposite("ab", "AB"))
+        self.assertTrue(isOpposite('ab', 'AB'))
 
     def test_true_2(self):
-        self.assertTrue(isOpposite("aB", "Ab"))
+        self.assertTrue(isOpposite('aB', 'Ab'))
 
     def test_true_3(self):
-        self.assertTrue(isOpposite("aBcd", "AbCD"))
+        self.assertTrue(isOpposite('aBcd', 'AbCD'))
 
     def test_false_1(self):
-        self.assertFalse(isOpposite("AB", "Ab"))
+        self.assertFalse(isOpposite('AB', 'Ab'))
 
     def test_false_2(self):
-        self.assertFalse(isOpposite("", ""))
+        self.assertFalse(isOpposite('', ''))
