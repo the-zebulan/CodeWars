35c5f36bdb5505cee8f7a6e0a61f04119f453ec0
the-zebulan <the-zebulan@users.noreply.github.com>
2016-08-02 12:33:43 -0600

    Added 'are_they_opposite' and unittests

---
+++ katas/kyu_8/are_they_opposite.py
@@ -0,0 +1,2 @@
+def isOpposite(s1, s2):
+    return False if not(s1 or s2) else s1.swapcase() == s2
---
+++ tests/kyu_8_tests/test_are_they_opposite.py
@@ -0,0 +1,20 @@
+import unittest
+
+from katas.kyu_8.are_they_opposite import isOpposite
+
+
+class IsOppositeTestCase(unittest.TestCase):
+    def test_true_1(self):
+        self.assertTrue(isOpposite("ab", "AB"))
+
+    def test_true_2(self):
+        self.assertTrue(isOpposite("aB", "Ab"))
+
+    def test_true_3(self):
+        self.assertTrue(isOpposite("aBcd", "AbCD"))
+
+    def test_false_1(self):
+        self.assertFalse(isOpposite("AB", "Ab"))
+
+    def test_false_2(self):
+        self.assertFalse(isOpposite("", ""))
