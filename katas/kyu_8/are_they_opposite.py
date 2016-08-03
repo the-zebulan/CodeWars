40acc79220f1ecc08858204f8b110669e7e56a4b
the-zebulan <the-zebulan@users.noreply.github.com>
2016-08-02 20:53:57 -0600

    Kata function was renamed using snake_case (PEP8)

--- katas/kyu_8/are_they_opposite.py
+++ katas/kyu_8/are_they_opposite.py
@@ -1,3 +1,2 @@
-def isOpposite(s1, s2):
-    """ is_opposite == PEP8 (forced mixedCase by Codewars) """
+def is_opposite(s1, s2):
     return False if not(s1 or s2) else s1.swapcase() == s2
--- tests/kyu_8_tests/test_are_they_opposite.py
+++ tests/kyu_8_tests/test_are_they_opposite.py
@@ -1,20 +1,20 @@
 import unittest
 
-from katas.kyu_8.are_they_opposite import isOpposite
+from katas.kyu_8.are_they_opposite import is_opposite
 
 
 class IsOppositeTestCase(unittest.TestCase):
     def test_true_1(self):
-        self.assertTrue(isOpposite('ab', 'AB'))
+        self.assertTrue(is_opposite('ab', 'AB'))
 
     def test_true_2(self):
-        self.assertTrue(isOpposite('aB', 'Ab'))
+        self.assertTrue(is_opposite('aB', 'Ab'))
 
     def test_true_3(self):
-        self.assertTrue(isOpposite('aBcd', 'AbCD'))
+        self.assertTrue(is_opposite('aBcd', 'AbCD'))
 
     def test_false_1(self):
-        self.assertFalse(isOpposite('AB', 'Ab'))
+        self.assertFalse(is_opposite('AB', 'Ab'))
 
     def test_false_2(self):
-        self.assertFalse(isOpposite('', ''))
+        self.assertFalse(is_opposite('', ''))
