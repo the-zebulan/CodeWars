8d0e65e6df9082d496ebd36120bd799d46d7d614
the-zebulan <the-zebulan@users.noreply.github.com>
2016-08-02 20:25:54 -0600

    Minor refactor to handle all possibilities of positive/negative combinations

--- katas/kyu_7/greatest_difference.py
+++ katas/kyu_7/greatest_difference.py
@@ -1,8 +1,11 @@
+from re import match
+
+
 def diff(pairs):
     max_diff = max_pair = False
     for pair in pairs:
-        a, _, b = pair.partition('-')
-        current = abs(int(a) - int(b))
+        a, b = (int(c) for c in match(r'(-?\d+)-(-?\d+)$', pair).groups())
+        current = abs(a - b)
         if current > max_diff:
             max_diff = current
             max_pair = pair
