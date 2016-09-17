807bb08dcc088956f8a27f4b1f00a00427974c17
the-zebulan <the-zebulan@users.noreply.github.com>
2016-09-16 21:27:24 -0600

    Refactored thanks to a solution on Codewars

--- katas/kyu_7/linked_lists_push_and_build_123.py
+++ katas/kyu_7/linked_lists_push_and_build_123.py
@@ -1,25 +1,12 @@
 class Node(object):
-    def __init__(self, data):
+    def __init__(self, data, nxt=None):
         self.data = data
-        self.next = None
+        self.next = nxt
 
 
 def push(head, data):
-    if head is None:
-        return Node(data)
-    else:
-        n = Node(data)
-        n.next = head
-        return n
+    return Node(data, head)
 
 
-def build_one_two_three(n=3):
-    node = None
-    for data in xrange(n, 0, -1):
-        if node is None:
-            node = Node(data)
-        else:
-            new = Node(data)
-            new.next = node
-            node = new
-    return node
+def build_one_two_three():
+    return Node(1, Node(2, Node(3)))
