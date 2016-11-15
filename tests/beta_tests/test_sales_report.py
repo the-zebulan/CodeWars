e3820fb3c89768afd96a826aa759a5d63fd30837
the-zebulan <the-zebulan@users.noreply.github.com>
2016-11-15 11:10:44 -0700

    Fixed multi-line string indentation with `textwrap.dedent`

--- tests/beta_tests/test_sales_report.py
+++ tests/beta_tests/test_sales_report.py
@@ -1,4 +1,5 @@
 import unittest
+from textwrap import dedent
 
 from katas.beta.sales_report import generate_report
 
@@ -19,33 +20,33 @@ class GenerateReportTestCase(unittest.TestCase):
                          'Total:                               0')
 
     def test_equal_2(self):
-        self.assertEqual(generate_report(self.records[:1]), '''\
-Group: 001
-    Product: 0001 Value:     12
-    Group total:                    12
+        self.assertEqual(generate_report(self.records[:1]), dedent('''\
+            Group: 001
+                Product: 0001 Value:     12
+                Group total:                    12
 
-Total:                              12''')
+            Total:                              12'''))
 
     def test_equal_3(self):
-        self.assertEqual(generate_report(self.records[:2]), '''\
-Group: 001
-    Product: 0001 Value:     12
-    Product: 0012 Value:   1000
-    Group total:                  1012
+        self.assertEqual(generate_report(self.records[:2]), dedent('''\
+            Group: 001
+                Product: 0001 Value:     12
+                Product: 0012 Value:   1000
+                Group total:                  1012
 
-Total:                            1012''')
+            Total:                            1012'''))
 
     def test_equal_4(self):
-        self.assertEqual(generate_report(self.records), '''\
-Group: 001
-    Product: 0001 Value:     12
-    Product: 0012 Value:   1032
-    Group total:                  1044
+        self.assertEqual(generate_report(self.records), dedent('''\
+            Group: 001
+                Product: 0001 Value:     12
+                Product: 0012 Value:   1032
+                Group total:                  1044
 
-Group: 007
-    Product: 0027 Value:    207
-    Product: 0112 Value:  12119
-    Product: 1009 Value:    200
-    Group total:                 12526
+            Group: 007
+                Product: 0027 Value:    207
+                Product: 0112 Value:  12119
+                Product: 1009 Value:    200
+                Group total:                 12526
 
-Total:                           13570''')
+            Total:                           13570'''))
