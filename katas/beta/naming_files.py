1769c7c7569023a3068b85b74ea1ecaa78f4bdf9
the-zebulan <the-zebulan@users.noreply.github.com>
2016-11-16 19:59:35 -0700

    Used 'xrange' instead (kata wasn't previously compatible with Python 2)

--- katas/beta/naming_files.py
+++ katas/beta/naming_files.py
@@ -2,4 +2,4 @@ def name_file(fmt, nbr, start):
     if not isinstance(nbr, int) or not isinstance(start, int) or nbr <= 0:
         return []
     filename = fmt.replace('<index_no>', '{0}').format
-    return [filename(a) for a in range(start, start + nbr)]
+    return [filename(a) for a in xrange(start, start + nbr)]
