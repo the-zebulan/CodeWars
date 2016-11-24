9775a9569ce6998dcd44a14e4dac7a2345088be0
the-zebulan <the-zebulan@users.noreply.github.com>
2016-11-23 21:45:23 -0700

    Refactored thanks to a solution on Codewars by MMMAAANNN

--- katas/kyu_4/snail.py
+++ katas/kyu_4/snail.py
@@ -1,29 +1,9 @@
-from itertools import cycle
-
-
 def snail(arr):
-    if arr == [[]]:
-        return []
-    direction = cycle('RDLU')
-    x_top = y_left = 0
-    x_bottom = y_right = len(arr) - 1
-    total_elements = len(arr) ** 2
+    """ Inspired by a solution on Codewars by MMMAAANNN """
+    matrix = list(arr)
     result = []
-    while len(result) < total_elements:
-        current_direction = next(direction)
-        if current_direction == 'R':
-            result.extend(arr[x_top][a] for a in xrange(y_left, y_right + 1))
-            x_top += 1
-        elif current_direction == 'D':
-            result.extend(
-                arr[b][y_right] for b in xrange(x_top, x_bottom + 1))
-            y_right -= 1
-        elif current_direction == 'L':
-            result.extend(
-                arr[x_bottom][c] for c in xrange(y_right, y_left - 1, -1))
-            x_bottom -= 1
-        elif current_direction == 'U':
-            result.extend(
-                arr[d][y_left] for d in xrange(x_bottom, x_top - 1, -1))
-            y_left += 1
+    while matrix:
+        result.extend(matrix.pop(0))
+        matrix = zip(*matrix)
+        matrix.reverse()
     return result
